8bd0fe94884501ef14c063fdfcaa21f674218493
Toru Shibamiya <shibamiya@qunasys.com>
2023-01-25 16:07:19 +0900

    Restructure

--- packages/core/quri_parts/core/utils/differentiation.py
+++ packages/core/quri_parts/core/utils/differentiation.py
@@ -8,275 +8,177 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Callable, Optional, Sequence
-
-from typing_extensions import TypeAlias
-
-FirstOrderDifferenceFormula: TypeAlias = Callable[
-    [Callable[[Sequence[float]], float], Sequence[float]],
-    Sequence[float],
-]
-SecondOrderDifferenceFormula: TypeAlias = Callable[
-    [Callable[[Sequence[float]], float], Sequence[float]],
-    Sequence[Sequence[float]],
-]
+from typing import Callable, Sequence
 
 
-def create_forward_difference_formula_gradient(
-    step: float = 1e-5,
-) -> FirstOrderDifferenceFormula:
-    """Create the function that returns the gradient of a passed function from
-    `params` with two-point forward-difference formula."""
-
-    def _diff_formula(
-        f: Callable[[Sequence[float]], float],
-        params: Sequence[float],
-    ) -> Sequence[float]:
-        dim = len(params)
-        forward_points = [
-            [params[k] + step * (k == i) for k in range(dim)] for i in range(dim)
-        ]
-        f_vals_forward = [f(forward) for forward in forward_points]
-        f_val_orig = f(params)
-
-        return [(f_val_fw - f_val_orig) / step for f_val_fw in f_vals_forward]
-
-    return _diff_formula
-
-
-def create_backward_difference_formula_gradient(
-    step: float = 1e-5,
-) -> FirstOrderDifferenceFormula:
-    """Create the function that returns the gradient of a passed function from
-    `params` with two-point backward-difference formula."""
-
-    def _diff_formula(
-        f: Callable[[Sequence[float]], float],
-        params: Sequence[float],
-    ) -> Sequence[float]:
-        dim = len(params)
-        backward_points = [
-            [params[k] - step * (k == i) for k in range(dim)] for i in range(dim)
-        ]
-        f_vals_backward = [f(backward) for backward in backward_points]
-        f_val_orig = f(params)
-
-        return [(f_val_orig - f_val_bw) / step for f_val_bw in f_vals_backward]
-
-    return _diff_formula
-
-
-def create_central_difference_formula_gradient(
-    step: float = 1e-5,
-) -> FirstOrderDifferenceFormula:
-    """Create the function that returns the gradient of a passed function from
-    `params` with central-difference formula."""
-
-    def _diff_formula(
-        f: Callable[[Sequence[float]], float],
-        params: Sequence[float],
-    ) -> Sequence[float]:
-        dim = len(params)
-        forward_points = [
-            [params[k] + step * (k == i) for k in range(dim)] for i in range(dim)
-        ]
-        backward_points = [
-            [params[k] - step * (k == i) for k in range(dim)] for i in range(dim)
-        ]
-        f_vals_forward = [f(forward) for forward in forward_points]
-        f_vals_backward = [f(backward) for backward in backward_points]
-
-        return [
-            (f_val_fw - f_val_bw) / (2 * step)
-            for f_val_fw, f_val_bw in zip(f_vals_forward, f_vals_backward)
-        ]
-
-    return _diff_formula
-
-
-def create_forward_difference_formula_hessian(
-    step: float = 1e-5,
-) -> SecondOrderDifferenceFormula:
-    """Create the function that returns the hessian of a passed function from
-    `params` with forward-difference formula."""
-
-    def _diff_formula(
-        f: Callable[[Sequence[float]], float],
-        params: Sequence[float],
-    ) -> Sequence[Sequence[float]]:
-        dim = len(params)
-        points_pp = [
-            [
-                [params[k] + step * (k == i) + step * (k == j) for k in range(dim)]
-                for j in range(dim)
-            ]
-            for i in range(dim)
-        ]
-        points_p = [
-            [params[k] + step * (k == i) for k in range(dim)] for i in range(dim)
-        ]
-        f_vals_pp = [[f(point_ij) for point_ij in points_i] for points_i in points_pp]
-        f_vals_p = [f(point_i) for point_i in points_p]
-        f_val_orig = f(params)
-
-        ret = [[0.0] * dim for _ in range(dim)]
-        for i in range(dim):
-            for j in range(dim):
-                ret[i][j] = (
-                    f_vals_pp[i][j] - f_vals_p[i] - f_vals_p[j] + f_val_orig
-                ) / (step * step)
-
-        return ret
-
-    return _diff_formula
-
-
-def create_backward_difference_formula_hessian(
-    step: float = 1e-5,
-) -> SecondOrderDifferenceFormula:
-    """Create the function that returns the hessian of a passed function from
-    `params` with backward-difference formula."""
-
-    def _diff_formula(
-        f: Callable[[Sequence[float]], float],
-        params: Sequence[float],
-    ) -> Sequence[Sequence[float]]:
-        dim = len(params)
-        points_mm = [
-            [
-                [params[k] - step * (k == i) - step * (k == j) for k in range(dim)]
-                for j in range(dim)
-            ]
-            for i in range(dim)
-        ]
-        points_m = [
-            [params[k] - step * (k == i) for k in range(dim)] for i in range(dim)
-        ]
-        f_vals_mm = [[f(point_ij) for point_ij in points_i] for points_i in points_mm]
-        f_vals_m = [f(point_i) for point_i in points_m]
-        f_val_orig = f(params)
-
-        ret = [[0.0] * dim for _ in range(dim)]
-        for i in range(dim):
-            for j in range(dim):
-                ret[i][j] = (
-                    f_val_orig - f_vals_m[i] - f_vals_m[j] + f_vals_mm[i][j]
-                ) / (step * step)
-
-        return ret
-
-    return _diff_formula
-
-
-def create_central_difference_formula_hessian(
-    step: float = 1e-5,
-) -> SecondOrderDifferenceFormula:
-    """Create the function that returns the hessian of a passed function from
-    `params` with central-difference formula."""
-
-    def _diff_formula(
-        f: Callable[[Sequence[float]], float],
-        params: Sequence[float],
-    ) -> Sequence[Sequence[float]]:
-        dim = len(params)
-        points_pp = [
-            [
-                [params[k] + step * (k == i) + step * (k == j) for k in range(dim)]
-                for j in range(dim)
-            ]
-            for i in range(dim)
-        ]
-        points_pm = [
-            [
-                [params[k] + step * (k == i) - step * (k == j) for k in range(dim)]
-                for j in range(dim)
-            ]
-            for i in range(dim)
-        ]
-        points_mp = [
-            [
-                [params[k] - step * (k == i) + step * (k == j) for k in range(dim)]
-                for j in range(dim)
-            ]
-            for i in range(dim)
-        ]
-        points_mm = [
-            [
-                [params[k] - step * (k == i) - step * (k == j) for k in range(dim)]
-                for j in range(dim)
-            ]
-            for i in range(dim)
-        ]
-
-        f_vals_pp = [[f(point_ij) for point_ij in points_i] for points_i in points_pp]
-        f_vals_pm = [[f(point_ij) for point_ij in points_i] for points_i in points_pm]
-        f_vals_mp = [[f(point_ij) for point_ij in points_i] for points_i in points_mp]
-        f_vals_mm = [[f(point_ij) for point_ij in points_i] for points_i in points_mm]
-
-        ret = [[0.0] * dim for _ in range(dim)]
-        for i in range(dim):
-            for j in range(dim):
-                ret[i][j] = (
-                    f_vals_pp[i][j]
-                    - f_vals_pm[i][j]
-                    - f_vals_mp[i][j]
-                    + f_vals_mm[i][j]
-                ) / (4 * step * step)
-
-        return ret
-
-    return _diff_formula
-
-
-def gradient(
+def forward_difference_gradient_formula(
     f: Callable[[Sequence[float]], float],
     params: Sequence[float],
-    step: float = 1e-3,
-    *,
-    difference_formula: Optional[FirstOrderDifferenceFormula] = None,
+    step: float = 1e-5,
 ) -> Sequence[float]:
-    """Returns the gradient of a passed function from given `params`.
+    """Returns the gradient of a passed function from `params` with two-point
+    forward-difference formula."""
+    dim = len(params)
+    forward_points = [
+        [params[k] + step * (k == i) for k in range(dim)] for i in range(dim)
+    ]
+    f_vals_forward = [f(forward) for forward in forward_points]
+    f_val_orig = f(params)
 
-    Args:
-        f: Function to find the gradient
-        params: Parameters from which the gradient is computed
-        step: Step size for numerical differentiation
-        difference_formula: Difference formula (keyward-only)
-    """
-
-    if not difference_formula:
-        difference_formula = create_central_difference_formula_gradient(step)
-    result = difference_formula(f, params)
-    if isinstance(result[0], list):
-        raise ValueError(
-            "Used incorrect formula for differentiation. Try using"
-            "the formula designed for gradient."
-        )
-    return result
+    return [(f_val_fw - f_val_orig) / step for f_val_fw in f_vals_forward]
 
 
-def hessian(
+def backward_difference_gradient_formula(
     f: Callable[[Sequence[float]], float],
     params: Sequence[float],
-    step: float = 1e-3,
-    *,
-    difference_formula: Optional[SecondOrderDifferenceFormula] = None,
+    step: float = 1e-5,
+) -> Sequence[float]:
+    """Returns the gradient of a passed function from `params` with two-point
+    backward-difference formula."""
+    dim = len(params)
+    backward_points = [
+        [params[k] - step * (k == i) for k in range(dim)] for i in range(dim)
+    ]
+    f_vals_backward = [f(backward) for backward in backward_points]
+    f_val_orig = f(params)
+
+    return [(f_val_orig - f_val_bw) / step for f_val_bw in f_vals_backward]
+
+
+def central_difference_gradient_formula(
+    f: Callable[[Sequence[float]], float],
+    params: Sequence[float],
+    step: float = 1e-5,
+) -> Sequence[float]:
+    """Returns the gradient of a passed function from `params` with central-
+    difference formula."""
+    dim = len(params)
+    forward_points = [
+        [params[k] + step * (k == i) for k in range(dim)] for i in range(dim)
+    ]
+    backward_points = [
+        [params[k] - step * (k == i) for k in range(dim)] for i in range(dim)
+    ]
+    f_vals_forward = [f(forward) for forward in forward_points]
+    f_vals_backward = [f(backward) for backward in backward_points]
+
+    return [
+        (f_val_fw - f_val_bw) / (2 * step)
+        for f_val_fw, f_val_bw in zip(f_vals_forward, f_vals_backward)
+    ]
+
+
+def forward_difference_hessian_formula(
+    f: Callable[[Sequence[float]], float],
+    params: Sequence[float],
+    step: float = 1e-5,
 ) -> Sequence[Sequence[float]]:
-    """Returns the hessian of a passed function from given `params`.
+    """Returns the hessian of a passed function from `params` with forward-
+    difference formula."""
+    dim = len(params)
+    points_pp = [
+        [
+            [params[k] + step * (k == i) + step * (k == j) for k in range(dim)]
+            for j in range(dim)
+        ]
+        for i in range(dim)
+    ]
+    points_p = [[params[k] + step * (k == i) for k in range(dim)] for i in range(dim)]
+    f_vals_pp = [[f(point_ij) for point_ij in points_i] for points_i in points_pp]
+    f_vals_p = [f(point_i) for point_i in points_p]
+    f_val_orig = f(params)
 
-    Args:
-        f: Function to find the hessian
-        params: Parameters from which the hessian is computed
-        step: Step size for numerical differentiation
-        difference_formula: Difference formula (keyward-only)
-    """
+    ret = [[0.0] * dim for _ in range(dim)]
+    for i in range(dim):
+        for j in range(dim):
+            ret[i][j] = (f_vals_pp[i][j] - f_vals_p[i] - f_vals_p[j] + f_val_orig) / (
+                step * step
+            )
 
-    if not difference_formula:
-        difference_formula = create_central_difference_formula_hessian(step)
-    result = difference_formula(f, params)
-    if isinstance(result[0], float):
-        raise ValueError(
-            "Used incorrect formula for differentiation. Try using"
-            "the formula designed for hessian."
-        )
-    return result
+    return ret
+
+
+def backward_difference_hessian_formula(
+    f: Callable[[Sequence[float]], float],
+    params: Sequence[float],
+    step: float = 1e-5,
+) -> Sequence[Sequence[float]]:
+    """Returns the hessian of a passed function from `params` with backward-
+    difference formula."""
+    dim = len(params)
+    points_mm = [
+        [
+            [params[k] - step * (k == i) - step * (k == j) for k in range(dim)]
+            for j in range(dim)
+        ]
+        for i in range(dim)
+    ]
+    points_m = [[params[k] - step * (k == i) for k in range(dim)] for i in range(dim)]
+    f_vals_mm = [[f(point_ij) for point_ij in points_i] for points_i in points_mm]
+    f_vals_m = [f(point_i) for point_i in points_m]
+    f_val_orig = f(params)
+
+    ret = [[0.0] * dim for _ in range(dim)]
+    for i in range(dim):
+        for j in range(dim):
+            ret[i][j] = (f_val_orig - f_vals_m[i] - f_vals_m[j] + f_vals_mm[i][j]) / (
+                step * step
+            )
+
+    return ret
+
+
+def central_difference_hessian_formula(
+    f: Callable[[Sequence[float]], float],
+    params: Sequence[float],
+    step: float = 1e-5,
+) -> Sequence[Sequence[float]]:
+    """Returns the hessian of a passed function from `params` with central-
+    difference formula."""
+    dim = len(params)
+    points_pp = [
+        [
+            [params[k] + step * (k == i) + step * (k == j) for k in range(dim)]
+            for j in range(dim)
+        ]
+        for i in range(dim)
+    ]
+    points_pm = [
+        [
+            [params[k] + step * (k == i) - step * (k == j) for k in range(dim)]
+            for j in range(dim)
+        ]
+        for i in range(dim)
+    ]
+    points_mp = [
+        [
+            [params[k] - step * (k == i) + step * (k == j) for k in range(dim)]
+            for j in range(dim)
+        ]
+        for i in range(dim)
+    ]
+    points_mm = [
+        [
+            [params[k] - step * (k == i) - step * (k == j) for k in range(dim)]
+            for j in range(dim)
+        ]
+        for i in range(dim)
+    ]
+
+    f_vals_pp = [[f(point_ij) for point_ij in points_i] for points_i in points_pp]
+    f_vals_pm = [[f(point_ij) for point_ij in points_i] for points_i in points_pm]
+    f_vals_mp = [[f(point_ij) for point_ij in points_i] for points_i in points_mp]
+    f_vals_mm = [[f(point_ij) for point_ij in points_i] for points_i in points_mm]
+
+    ret = [[0.0] * dim for _ in range(dim)]
+    for i in range(dim):
+        for j in range(dim):
+            ret[i][j] = (
+                f_vals_pp[i][j] - f_vals_pm[i][j] - f_vals_mp[i][j] + f_vals_mm[i][j]
+            ) / (4 * step * step)
+
+    return ret
+
+
+gradient = central_difference_gradient_formula
+hessian = central_difference_hessian_formula
--- packages/core/tests/core/utils/test_differentiation.py
+++ packages/core/tests/core/utils/test_differentiation.py
@@ -11,15 +11,14 @@
 from typing import Sequence
 
 import numpy as np
-import pytest
 
 from quri_parts.core.utils.differentiation import (
-    create_backward_difference_formula_gradient,
-    create_backward_difference_formula_hessian,
-    create_central_difference_formula_gradient,
-    create_central_difference_formula_hessian,
-    create_forward_difference_formula_gradient,
-    create_forward_difference_formula_hessian,
+    backward_difference_gradient_formula,
+    backward_difference_hessian_formula,
+    central_difference_gradient_formula,
+    central_difference_hessian_formula,
+    forward_difference_gradient_formula,
+    forward_difference_hessian_formula,
     gradient,
     hessian,
 )
@@ -37,119 +36,61 @@ def f(params: Sequence[float]) -> float:
     )
 
 
-def test_create_forward_differene_formula_gradient() -> None:
+def test_forward_differene_gradient_formula() -> None:
     params = [1, 2, 3]
-    diff_formula = create_forward_difference_formula_gradient()
     expected = [-6.4, 3.2, 6.3]
-    assert np.allclose(diff_formula(f, params), expected)
+    assert np.allclose(forward_difference_gradient_formula(f, params), expected)
 
 
-def test_create_backward_differene_formula_gradient() -> None:
+def test_backward_differene_gradient_formula() -> None:
     params = [1, 2, 3]
-    diff_formula = create_backward_difference_formula_gradient()
     expected = [-6.4, 3.2, 6.3]
-    assert np.allclose(diff_formula(f, params), expected)
+    assert np.allclose(backward_difference_gradient_formula(f, params), expected)
 
 
-def test_create_central_differene_formula_gradient() -> None:
+def test_central_differene_gradient_formula() -> None:
     params = [1, 2, 3]
-    diff_formula = create_central_difference_formula_gradient()
     expected = [-6.4, 3.2, 6.3]
-    assert np.allclose(diff_formula(f, params), expected)
+    assert np.allclose(central_difference_gradient_formula(f, params), expected)
 
 
-def test_create_forward_differene_formula_hessian() -> None:
+def test_forward_differene_hessian_formula() -> None:
     params = [1, 2, 3]
-    diff_formula = create_forward_difference_formula_hessian()
     expected = [[2.2, -1.2, -4.2], [-1.2, 0.2, 2.4], [-4.2, 2.4, 4.0]]
-    assert np.allclose(diff_formula(f, params), expected, rtol=1e-3)
+    assert np.allclose(
+        forward_difference_hessian_formula(f, params), expected, rtol=1e-3
+    )
 
 
-def test_create_backward_differene_formula_hessian() -> None:
+def test_backward_differene_hessian_formula() -> None:
     params = [1, 2, 3]
-    diff_formula = create_backward_difference_formula_hessian()
     expected = [[2.2, -1.2, -4.2], [-1.2, 0.2, 2.4], [-4.2, 2.4, 4.0]]
-    assert np.allclose(diff_formula(f, params), expected, rtol=1e-3)
+    assert np.allclose(
+        backward_difference_hessian_formula(f, params), expected, rtol=1e-3
+    )
 
 
-def test_create_2nd_order_central_differene_formula() -> None:
+def test_central_differene_hessian_formula() -> None:
     params = [1, 2, 3]
-    diff_formula = create_central_difference_formula_hessian()
     expected = [[2.2, -1.2, -4.2], [-1.2, 0.2, 2.4], [-4.2, 2.4, 4.0]]
-    assert np.allclose(diff_formula(f, params), expected, rtol=1e-3)
+    assert np.allclose(
+        central_difference_hessian_formula(f, params), expected, rtol=1e-3
+    )
 
 
 def test_gradient() -> None:
     params = [1, 2, 3]
-    step = 1e-5
-    expected = [-6.4, 3.2, 6.3]
-    assert np.allclose(gradient(f, params, step=step), expected)
-
-    fdf = create_forward_difference_formula_gradient(step)
-    bdf = create_backward_difference_formula_gradient(step)
-    cdf = create_central_difference_formula_gradient(step)
-
-    fdf_res = gradient(f, params, step=step, difference_formula=fdf)
-    bdf_res = gradient(f, params, step=step, difference_formula=bdf)
-    cdf_res = gradient(f, params, step=step, difference_formula=cdf)
-
-    assert fdf_res != bdf_res
-    assert np.allclose(fdf_res, bdf_res)
-    assert fdf_res != cdf_res
-    assert np.allclose(fdf_res, bdf_res)
-    assert bdf_res != cdf_res
-
-
-def test_gradient_invalid_formulas() -> None:
-    params = [1, 2, 3]
-
-    fdf_hessian = create_forward_difference_formula_hessian()
-    bdf_hessian = create_backward_difference_formula_hessian()
-    cdf_hessian = create_central_difference_formula_hessian()
-
-    with pytest.raises(ValueError):
-        gradient(f, params, difference_formula=fdf_hessian)  # type: ignore
-
-    with pytest.raises(ValueError):
-        gradient(f, params, difference_formula=bdf_hessian)  # type: ignore
-
-    with pytest.raises(ValueError):
-        gradient(f, params, difference_formula=cdf_hessian)  # type: ignore
+    step = 1e-3
+    assert np.allclose(
+        gradient(f, params, step),
+        central_difference_gradient_formula(f, params, step=step),
+    )
 
 
 def test_hessian() -> None:
     params = [1, 2, 3]
-    step = 1e-5
-    expected = [[2.2, -1.2, -4.2], [-1.2, 0.2, 2.4], [-4.2, 2.4, 4.0]]
-    assert np.allclose(hessian(f, params, step=1e-3), expected)
-
-    fdf = create_forward_difference_formula_hessian(step)
-    bdf = create_backward_difference_formula_hessian(step)
-    cdf = create_central_difference_formula_hessian(step)
-
-    fdf_res = hessian(f, params, step=step, difference_formula=fdf)
-    bdf_res = hessian(f, params, step=step, difference_formula=bdf)
-    cdf_res = hessian(f, params, step=step, difference_formula=cdf)
-
-    assert fdf_res != bdf_res
-    assert np.allclose(fdf_res, bdf_res, rtol=1e-3)
-    assert fdf_res != cdf_res
-    assert np.allclose(fdf_res, bdf_res, rtol=1e-3)
-    assert bdf_res != cdf_res
-
-
-def test_hessian_invalid_formulas() -> None:
-    params = [1, 2, 3]
-
-    fdf_gradient = create_forward_difference_formula_gradient()
-    bdf_gradient = create_backward_difference_formula_gradient()
-    cdf_gradient = create_central_difference_formula_gradient()
-
-    with pytest.raises(ValueError):
-        hessian(f, params, difference_formula=fdf_gradient)  # type: ignore
-
-    with pytest.raises(ValueError):
-        hessian(f, params, difference_formula=bdf_gradient)  # type: ignore
-
-    with pytest.raises(ValueError):
-        hessian(f, params, difference_formula=cdf_gradient)  # type: ignore
+    step = 1e-3
+    assert np.allclose(
+        hessian(f, params, step),
+        central_difference_hessian_formula(f, params, step=step),
+    )
