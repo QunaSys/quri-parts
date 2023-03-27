49a4d4a45a888bae3426e8d23736b208a4419ee1
speed1313 <speedtry13@icloud.com>
2023-03-27 10:05:14 +0900

    rename s symbol

--- packages/itensor/quri_parts/itensor/operator/__init__.py
+++ packages/itensor/quri_parts/itensor/operator/__init__.py
@@ -8,10 +8,12 @@ from quri_parts.core.operator import Operator, PauliLabel, pauli_name
 
 
 def convert_operator(
-    operator: Union[Operator, PauliLabel], s: juliacall.VectorValue
+    operator: Union[Operator, PauliLabel], qubit_sites: juliacall.VectorValue
 ) -> juliacall.AnyValue:
     """Convert an :class:`~Operator` or a :class:`~PauliLabel` to an ITensor
-    MPO operator."""
+    MPO operator.
+    qubit_sites: collection of N "Qubit" sites. please follow [the Itensor doc](https://itensor.github.io/ITensors.jl/stable/IncludedSiteTypes.html#%22Qubit%22-SiteType)
+    """
     paulis: Iterable[tuple[PauliLabel, complex]]
     if isinstance(operator, Operator):
         paulis = operator.items()
@@ -21,10 +23,10 @@ def convert_operator(
     for pauli, coef in paulis:
         pauli_gates: juliacall.VectorValue = jl.gate_list()
         if len(pauli) == 0:
-            os = jl.add_coef_identity(os, coef, s)
+            os = jl.add_coef_identity(os, coef, qubit_sites)
             continue
         for i, p in pauli:
             pauli_gates = jl.add_pauli(pauli_gates, pauli_name(p), i + 1)
         os = jl.add_coef_pauli(os, coef, pauli_gates)
-    op: juliacall.AnyValue = jl.MPO(os, s)
+    op: juliacall.AnyValue = jl.MPO(os, qubit_sites)
     return op
