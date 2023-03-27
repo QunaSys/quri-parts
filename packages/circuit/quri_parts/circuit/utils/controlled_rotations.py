6659fd68eaf713bbbafbfbf08da3d6a6a02d8467
Toru Shibamiya <shibamiya@qunasys.com>
2023-03-27 21:25:29 +0900

    Replace the argument of add_controlled_RY_gate

--- packages/circuit/quri_parts/circuit/utils/controlled_rotations.py
+++ packages/circuit/quri_parts/circuit/utils/controlled_rotations.py
@@ -1,13 +1,20 @@
-from .. import LinearMappedUnboundParametricQuantumCircuit, LinearParameterFunction
+from .. import (
+    LinearMappedUnboundParametricQuantumCircuit,
+    Parameter,
+    ParameterOrLinearFunction,
+)
 
 
 def add_controlled_RY_gate(
     circuit: LinearMappedUnboundParametricQuantumCircuit,
     control_index: int,
     target_index: int,
-    param_fn: LinearParameterFunction,
+    param_fn: ParameterOrLinearFunction,
 ) -> LinearMappedUnboundParametricQuantumCircuit:
-    inv_sign_param_fn = {param: -1.0 * val for param, val in param_fn.items()}
+    if isinstance(param_fn, Parameter):
+        inv_sign_param_fn = {param_fn: -1.0}
+    else:
+        inv_sign_param_fn = {param: -1.0 * val for param, val in param_fn.items()}
     circuit.add_ParametricRY_gate(target_index, param_fn)
     circuit.add_CNOT_gate(control_index, target_index)
     circuit.add_ParametricRY_gate(target_index, inv_sign_param_fn)
--- packages/circuit/tests/circuit/utils/test_controlled_rotations.py
+++ packages/circuit/tests/circuit/utils/test_controlled_rotations.py
@@ -2,9 +2,27 @@ from quri_parts.circuit import LinearMappedUnboundParametricQuantumCircuit
 from quri_parts.circuit.utils.controlled_rotations import add_controlled_RY_gate
 
 
-def test_add_controlled_Y_gate() -> None:
+def test_add_controlled_RY_gate() -> None:
     qubit_count = 4
     excitation = (0, 2)
+    circuit = LinearMappedUnboundParametricQuantumCircuit(qubit_count)
+    theta = circuit.add_parameter("theta")
+    add_controlled_RY_gate(circuit, excitation[1], excitation[0], theta)
+    expected_circuit = LinearMappedUnboundParametricQuantumCircuit(qubit_count)
+    exp_theta = expected_circuit.add_parameter("theta")
+    expected_circuit.add_ParametricRY_gate(excitation[0], {exp_theta: 1.0})
+    expected_circuit.add_CNOT_gate(excitation[1], excitation[0])
+    expected_circuit.add_ParametricRY_gate(excitation[0], {exp_theta: -1.0})
+    expected_circuit.add_CNOT_gate(excitation[1], excitation[0])
+    assert circuit.parameter_count == expected_circuit.parameter_count
+    assert circuit._circuit.gates == expected_circuit._circuit.gates
+    params = [0.1 * (i + 1) for i in range(circuit.parameter_count)]
+    bound_circuit = circuit.bind_parameters(params)
+    expected_bound_circuit = expected_circuit.bind_parameters(params)
+    assert bound_circuit == expected_bound_circuit
+
+    qubit_count = 4
+    excitation = (1, 3)
     circuit = LinearMappedUnboundParametricQuantumCircuit(qubit_count)
     theta = circuit.add_parameter("theta")
     add_controlled_RY_gate(circuit, excitation[1], excitation[0], {theta: 0.5})
