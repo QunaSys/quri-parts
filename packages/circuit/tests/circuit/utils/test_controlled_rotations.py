a55920de2f88930dba86031994398567676b5dc0
Toru Shibamiya <shibamiya@qunasys.com>
2023-03-27 21:02:48 +0900

    Refine tests

--- packages/circuit/tests/circuit/utils/test_controlled_rotations.py
+++ packages/circuit/tests/circuit/utils/test_controlled_rotations.py
@@ -25,12 +25,12 @@ def test_add_controlled_Y_gate() -> None:
     excitation = (1, 3)
     circuit = LinearMappedUnboundParametricQuantumCircuit(qubit_count)
     theta = circuit.add_parameter("theta")
-    add_controlled_RY_gate(circuit, excitation[1], excitation[0], {theta: 0.5})
+    add_controlled_RY_gate(circuit, excitation[1], excitation[0], {theta: -0.5})
     expected_circuit = LinearMappedUnboundParametricQuantumCircuit(qubit_count)
     exp_theta = expected_circuit.add_parameter("theta")
-    expected_circuit.add_ParametricRY_gate(excitation[0], {exp_theta: 0.5})
-    expected_circuit.add_CNOT_gate(excitation[1], excitation[0])
     expected_circuit.add_ParametricRY_gate(excitation[0], {exp_theta: -0.5})
+    expected_circuit.add_CNOT_gate(excitation[1], excitation[0])
+    expected_circuit.add_ParametricRY_gate(excitation[0], {exp_theta: 0.5})
     expected_circuit.add_CNOT_gate(excitation[1], excitation[0])
     assert circuit.parameter_count == expected_circuit.parameter_count
     assert circuit._circuit.gates == expected_circuit._circuit.gates
