15ab69c3ef691b996a42cc4c2e367577b923fc4d
Daniel <chung@qunasys.com>
2023-04-14 13:21:52 +0800

    Fix type errors in gate_converter.py

--- packages/qulacs/quri_parts/qulacs/circuit/noise/gate_converter.py
+++ packages/qulacs/quri_parts/qulacs/circuit/noise/gate_converter.py
@@ -14,31 +14,31 @@ from qulacs import QuantumGateBase
 from qulacs.gate import CPTP, DenseMatrix, Identity, Pauli, Probabilistic
 
 from quri_parts.circuit.noise import AbstractKrausNoise, PauliNoise, ProbabilisticNoise
-
+from typing import cast
 
 def create_kraus_gate(
     qubits: Sequence[int], noise: AbstractKrausNoise
 ) -> QuantumGateBase:
-    return CPTP([DenseMatrix(qubits, kraus) for kraus in noise.kraus_operators])
+    return CPTP([DenseMatrix(qubits, kraus) for kraus in noise.kraus_operators])  # type: ignore
 
 
 def create_pauli_noise_gate(
     qubits: Sequence[int], noise: PauliNoise
 ) -> QuantumGateBase:
-    qubits = noise.qubit_indices
+    qubits = list(noise.qubit_indices)
     pauli_list, prob_list = noise.pauli_list, list(noise.prob_list)
 
-    pauli_gates = [Pauli(qubits, pauli) for pauli in pauli_list]
+    pauli_gates = [Pauli(qubits, list(pauli)) for pauli in pauli_list]
     if sum(prob_list) < 1.0:
         pauli_gates.append(Identity(len(qubits)))
         prob_identity = 1.0 - sum(prob_list)
         prob_list.append(prob_identity)
 
-    return Probabilistic(prob_list, pauli_gates)
+    return cast(QuantumGateBase, Probabilistic(prob_list, pauli_gates))
 
 
 def create_probabilistic_gate(
     qubits: Sequence[int], noise: ProbabilisticNoise
 ) -> QuantumGateBase:
-    dense_matrices = [DenseMatrix(qubits, matrix) for matrix in noise.gate_matrices]
-    return Probabilistic(noise.prob_list, dense_matrices)
+    dense_matrices = [DenseMatrix(qubits, matrix) for matrix in noise.gate_matrices]  # type: ignore
+    return cast(QuantumGateBase, Probabilistic(list(noise.prob_list), dense_matrices))
