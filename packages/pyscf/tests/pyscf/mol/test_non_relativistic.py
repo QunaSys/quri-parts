bc7af7e781cac8fc023c188b162a01e89a922e92
Daniel <chung@qunasys.com>
2023-04-18 11:16:46 +0800

    fix linting problem

--- packages/pyscf/tests/pyscf/mol/test_non_relativistic.py
+++ packages/pyscf/tests/pyscf/mol/test_non_relativistic.py
@@ -2,11 +2,7 @@ from numpy import isclose
 from pyscf import gto, scf
 
 from quri_parts.pyscf.mol import (
-    PySCFAO1eInt,
-    PySCFAO2eInt,
     PySCFMolecularOrbitals,
-    ao1int,
-    ao2int,
     get_ao_eint_set,
     pyscf_get_ao_eint_set,
 )
@@ -31,8 +27,7 @@ hcl = PySCFMolecularOrbitals(mol=hcl_mol, mo_coeff=hcl_mf.mo_coeff)
 
 
 def qp_chem_qp_pyscf_comparison(molecule: PySCFMolecularOrbitals) -> None:
-    """ Compare output of AOeIntArraySet and PySCFAOeIntSet
-    """
+    """Compare output of AOeIntArraySet and PySCFAOeIntSet."""
     qp_chem_ao_eint_set = get_ao_eint_set(molecule)
     qp_chem_ao1eint = qp_chem_ao_eint_set.ao_1e_int
     qp_chem_spin_mo1eint = qp_chem_ao1eint.to_mo1int(molecule.mo_coeff)
@@ -53,8 +48,13 @@ def qp_chem_qp_pyscf_comparison(molecule: PySCFMolecularOrbitals) -> None:
     assert isclose(qp_chem_ao2eint.array - qp_pyscf_ao2eint.array, 0).all()
     assert isclose(qp_chem_spin_mo1eint.array - qp_pyscf_spin_mo1eint.array, 0).all()
     assert isclose(qp_chem_spin_mo2eint.array - qp_pyscf_spin_mo2eint.array, 0).all()
-    assert isclose(qp_chem_spatial_mo1eint.array - qp_pyscf_spatial_mo1eint.array, 0).all()
-    assert isclose(qp_chem_spatial_mo2eint.array - qp_pyscf_spatial_mo2eint.array, 0).all()
+    assert isclose(
+        qp_chem_spatial_mo1eint.array - qp_pyscf_spatial_mo1eint.array, 0
+    ).all()
+    assert isclose(
+        qp_chem_spatial_mo2eint.array - qp_pyscf_spatial_mo2eint.array, 0
+    ).all()
+
 
 def test_h2o() -> None:
     qp_chem_qp_pyscf_comparison(h2o)
