29ccaf300c89b02c7dc6c30bdfe47054c2eee9e0
Daniel <chung@qunasys.com>
2023-04-27 14:35:17 +0800

    Add test for converting basic gates

--- packages/tket/tests/tket/circuit/test_tket_convert_circuit.py
+++ packages/tket/tests/tket/circuit/test_tket_convert_circuit.py
@@ -2,15 +2,15 @@ from collections.abc import Mapping
 from typing import Callable, Type, cast
 
 import numpy as np
-
 from pytket import Circuit, OpType  # type: ignore
+
 from quri_parts.circuit import QuantumCircuit, QuantumGate, gates
-from quri_parts.tket.circuit import convert_circuit, convert_gate
+from quri_parts.tket.circuit import convert_circuit  # type: ignore
 
 
 def circuit_equal(c1: Circuit, c2: Circuit) -> bool:
     # Compare the matrix representation of the circuits, including the global phase.
-    return np.all(c1.get_unitary() == c2.get_unitary())
+    return cast(bool, np.all(c1.get_unitary() == c2.get_unitary()))
 
 
 single_qubit_gate_mapping: Mapping[Callable[[int], QuantumGate], OpType] = {
@@ -32,7 +32,7 @@ def test_convert_single_qubit_gate() -> None:
     for qp_factory, tket_gate in single_qubit_gate_mapping.items():
         target_index = 7
         n_qubit = 8
-        
+
         qp_gate = qp_factory(target_index)
         qp_circuit = QuantumCircuit(n_qubit)
         qp_circuit.add_gate(qp_gate)
@@ -63,9 +63,7 @@ def test_convert_single_qubit_gate() -> None:
 #         assert gate_equal(converted, expected)
 
 
-rotation_gate_mapping: Mapping[
-    Callable[[int, float], QuantumGate], Type[OpType]
-] = {
+rotation_gate_mapping: Mapping[Callable[[int, float], QuantumGate], Type[OpType]] = {
     gates.RX: OpType.Rx,
     gates.RY: OpType.Ry,
     gates.RZ: OpType.Rz,
@@ -74,50 +72,72 @@ rotation_gate_mapping: Mapping[
 
 def test_convert_rotation_gate() -> None:
     for qp_factory, tket_gate in rotation_gate_mapping.items():
-        
         target_index = 7
         n_qubit = 8
-        
+
         qp_gate = qp_factory(7, 0.125)
         qp_circuit = QuantumCircuit(n_qubit)
         qp_circuit.add_gate(qp_gate)
 
         tket_circuit = Circuit(n_qubit)
-        tket_circuit.add_gate(tket_gate, 0.125/np.pi, [target_index])
+        tket_circuit.add_gate(tket_gate, 0.125 / np.pi, [target_index])
 
         converted = convert_circuit(qp_circuit)
         expected = tket_circuit
         assert circuit_equal(converted, expected)
 
 
-# two_qubit_gate_mapping: Mapping[Callable[[int, int], QuantumGate], QiskitGate] = {
-#     gates.CNOT: qgate.CXGate(),
-#     gates.CZ: qgate.CZGate(),
-#     gates.SWAP: qgate.SwapGate(),
-# }
+two_qubit_gate_mapping: Mapping[Callable[[int, int], QuantumGate], OpType] = {
+    gates.CNOT: OpType.CX,
+    gates.CZ: OpType.CZ,
+    gates.SWAP: OpType.SWAP,
+}
 
 
-# def test_convert_two_qubit_gate() -> None:
-#     for qp_factory, qiskit_gate in two_qubit_gate_mapping.items():
-#         qp_gate = qp_factory(11, 7)
-#         converted = convert_gate(qp_gate)
-#         expected = qiskit_gate
-#         assert gate_equal(converted, expected)
+def test_convert_two_qubit_gate() -> None:
+    for qp_factory, tket_gate in two_qubit_gate_mapping.items():
+        target_index = 7
+        control_index = 4
+        n_qubit = 8
+
+        qp_gate = qp_factory(control_index, target_index)
+        qp_circuit = QuantumCircuit(n_qubit)
+        qp_circuit.add_gate(qp_gate)
+
+        tket_circuit = Circuit(n_qubit)
+        tket_circuit.add_gate(tket_gate, [control_index, target_index])
+
+        converted = convert_circuit(qp_circuit)
+        expected = tket_circuit
+
+        assert circuit_equal(converted, expected)
 
 
-# three_qubit_gate_mapping: Mapping[
-#     Callable[[int, int, int], QuantumGate], QiskitGate
-# ] = {
-#     gates.TOFFOLI: qgate.CCXGate(),
-# }
+three_qubit_gate_mapping: Mapping[Callable[[int, int, int], QuantumGate], OpType] = {
+    gates.TOFFOLI: OpType.CCX,
+}
 
 
-# def test_convert_three_qubit_gate() -> None:
-#     for qp_factory, qiskit_gate in three_qubit_gate_mapping.items():
-#         qp_gate = qp_factory(11, 7, 5)
-#         converted = convert_gate(qp_gate)
-#         expected = qiskit_gate
-#         assert gate_equal(converted, expected)
+def test_convert_three_qubit_gate() -> None:
+    for qp_factory, tket_gate in three_qubit_gate_mapping.items():
+        target_index = 7
+        control_index_1 = 4
+        control_index_2 = 2
+        n_qubit = 8
+
+        qp_gate = qp_factory(control_index_1, control_index_2, target_index)
+        qp_circuit = QuantumCircuit(n_qubit)
+        qp_circuit.add_gate(qp_gate)
+
+        tket_circuit = Circuit(n_qubit)
+        tket_circuit.add_gate(
+            tket_gate, [control_index_1, control_index_2, target_index]
+        )
+
+        converted = convert_circuit(qp_circuit)
+        expected = tket_circuit
+
+        assert circuit_equal(converted, expected)
 
 
 # def test_convert_unitary_matrix_gate() -> None:
@@ -128,48 +148,85 @@ def test_convert_rotation_gate() -> None:
 #     assert gate_equal(converted, expected)
 
 
-# def test_convert_u1_gate() -> None:
-#     lmd1 = 0.125
-#     converted = convert_gate(gates.U1(lmd=lmd1, target_index=0))
-#     expected = qgate.PhaseGate(lmd1)
-#     assert gate_equal(converted, expected)
+def test_convert_u1_gate() -> None:
+    lmd1 = 0.125
+
+    target_index = 7
+    n_qubit = 8
+
+    qp_gate = gates.U1(lmd=lmd1, target_index=target_index)
+    qp_circuit = QuantumCircuit(n_qubit)
+    qp_circuit.add_gate(qp_gate)
+
+    tket_circuit = Circuit(n_qubit)
+    tket_circuit.add_gate(OpType.U1, [lmd1 / np.pi], [target_index])
+
+    converted = convert_circuit(qp_circuit)
+    expected = tket_circuit
+
+    assert circuit_equal(converted, expected)
 
 
-# def test_convert_u2_gate() -> None:
-#     phi2, lmd2 = 0.125, -0.125
-#     converted = convert_gate(gates.U2(phi=phi2, lmd=lmd2, target_index=0))
-#     expected = qgate.UGate(np.pi / 2, phi2, lmd2)
-#     assert gate_equal(converted, expected)
+def test_convert_u2_gate() -> None:
+    phi2, lmd2 = 0.125, -0.125
+
+    target_index = 7
+    n_qubit = 8
+
+    qp_gate = gates.U2(lmd=lmd2, phi=phi2, target_index=target_index)
+    qp_circuit = QuantumCircuit(n_qubit)
+    qp_circuit.add_gate(qp_gate)
+
+    tket_circuit = Circuit(n_qubit)
+    tket_circuit.add_gate(OpType.U2, [phi2 / np.pi, lmd2 / np.pi], [target_index])
+
+    converted = convert_circuit(qp_circuit)
+    expected = tket_circuit
+
+    assert circuit_equal(converted, expected)
 
 
-# def test_convert_u3_gate() -> None:
-#     theta3, phi3, lmd3 = 0.125, -0.125, 0.625
-#     converted = convert_gate(gates.U3(theta=theta3, phi=phi3, lmd=lmd3, target_index=0))
-#     expected = qgate.UGate(theta3, phi3, lmd3)
-#     assert gate_equal(converted, expected)
+def test_convert_u3_gate() -> None:
+    theta3, phi3, lmd3 = 0.125, -0.125, 0.625
+    target_index = 7
+    n_qubit = 8
+
+    qp_gate = gates.U3(lmd=lmd3, phi=phi3, theta=theta3, target_index=target_index)
+    qp_circuit = QuantumCircuit(n_qubit)
+    qp_circuit.add_gate(qp_gate)
+
+    tket_circuit = Circuit(n_qubit)
+    tket_circuit.add_gate(
+        OpType.U3, [theta3 / np.pi, phi3 / np.pi, lmd3 / np.pi], [target_index]
+    )
+
+    converted = convert_circuit(qp_circuit)
+    expected = tket_circuit
+
+    assert circuit_equal(converted, expected)
 
 
-# def test_convert_circuit() -> None:
-#     circuit = QuantumCircuit(3)
-#     original_gates = [
-#         gates.X(1),
-#         gates.H(2),
-#         gates.CNOT(0, 2),
-#         gates.RX(0, 0.125),
-#     ]
-#     for gate in original_gates:
-#         circuit.add_gate(gate)
+def test_convert_circuit() -> None:
+    circuit = QuantumCircuit(3)
+    original_gates = [
+        gates.X(1),
+        gates.H(2),
+        gates.CNOT(0, 2),
+        gates.RX(0, 0.125),
+    ]
+    for gate in original_gates:
+        circuit.add_gate(gate)
 
-#     converted = convert_circuit(circuit)
-#     assert converted.num_qubits == 3
+    converted = convert_circuit(circuit)
+    assert converted.n_qubits == 3
 
-#     expected = QiskitQuantumCircuit(3)
-#     expected.x(1)
-#     expected.h(2)
-#     expected.cnot(0, 2)
-#     expected.rx(0.125, 0)
+    expected = Circuit(3)
+    expected.X(1)
+    expected.H(2)
+    expected.CX(0, 2)
+    expected.Rx(0.125 / np.pi, 0)
 
-#     assert circuit_equal(converted, expected)
+    assert circuit_equal(converted, expected)
 
 
 # def test_convert_pauli() -> None:
