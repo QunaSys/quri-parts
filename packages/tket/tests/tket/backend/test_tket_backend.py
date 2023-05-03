49be5f38c22a7943ab2c564dd87e0a775a3f5c98
Daniel <chung@qunasys.com>
2023-05-03 21:32:24 +0800

    Add backend test

--- packages/tket/tests/tket/backend/test_tket_backend.py
+++ packages/tket/tests/tket/backend/test_tket_backend.py
@@ -49,13 +49,3 @@ class TestTKetSamplingBackend(unittest.TestCase):
             0b0110,
             0b0111,
         }
-        assert set(self.result.counts.keys()) == {
-            0b0000,
-            0b0001,
-            0b0010,
-            0b0011,
-            0b0100,
-            0b0101,
-            0b0110,
-            0b0111,
-        }
