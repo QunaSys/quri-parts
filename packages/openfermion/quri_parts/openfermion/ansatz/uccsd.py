66fb5d897f438ccd3784b752fd26f3dceb51ba16
Daniel <chung@qunasys.com>
2023-05-15 10:53:50 +0800

    Fix linting errors

--- packages/openfermion/quri_parts/openfermion/ansatz/uccsd.py
+++ packages/openfermion/quri_parts/openfermion/ansatz/uccsd.py
@@ -42,7 +42,7 @@ class TrotterSingletUCCSD(ImmutableLinearMappedUnboundParametricQuantumCircuit):
           :class:`Operator`
         trotter_number: Number for first-order Trotter product formula.
         use_singles: If ``True``, single-excitation gates are applied.
-        spin_symmetric: If ``True``, the ansatz will be spin symmetric so that 
+        spin_symmetric: If ``True``, the ansatz will be spin symmetric so that
             excitations between the same spatial orbitals will share the same
             circuit parameter. For example, single excitation (0, 2) and (1, 3)
             share the same circuit parameter. Note that this option is only valid
