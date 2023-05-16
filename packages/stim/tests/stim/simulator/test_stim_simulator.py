555233b471f5f389bb91d875e57b9a44f0358124
Daniel <chung@qunasys.com>
2023-05-16 11:34:09 +0800

    Add license text and remove unnecessary print statements

--- packages/stim/tests/stim/simulator/test_stim_simulator.py
+++ packages/stim/tests/stim/simulator/test_stim_simulator.py
@@ -1,3 +1,13 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#      http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import unittest
 
 from numpy import allclose, angle, array, cfloat, exp
@@ -23,7 +33,7 @@ class TestSimulator(unittest.TestCase):
         cls.update_circuit.add_SqrtY_gate(0)
         cls.update_circuit.add_SqrtX_gate(1)
 
-        cls.expected_output = array([0.0, 0.0, -0.5-0.5j, -0.5-0.5j])
+        cls.expected_output = array([0.0, 0.0, -0.5 - 0.5j, -0.5 - 0.5j])
         # This is to cope with the fact that stim always canonicalize the
         # state vector such that the first non-zero component is positive.
         phase = exp(1j * angle(cls.expected_output[cls.expected_output != 0][0]))
@@ -33,7 +43,6 @@ class TestSimulator(unittest.TestCase):
         circuit_quantum_state = GeneralCircuitQuantumState(2, self.update_circuit)
         quantum_state_vector = evaluate_state_to_vector(circuit_quantum_state)
         assert len(quantum_state_vector.circuit.gates) == 0
-        print(quantum_state_vector.vector)
         assert allclose(quantum_state_vector.vector, [0, 0.70710678, 0, -0.70710678])
 
     def test_evaluate_state_to_vector_vec_to_vec(self) -> None:
