4cea21bb13a3df54ea3f52f88a68939a0dc7300e
Kentaro Wada <wada.k@qunasys.com>
2023-05-16 10:44:08 +0900

    add qiskit transpiler

---
+++ packages/qiskit/quri_parts/qiskit/circuit/transpile/__init__.py
@@ -0,0 +1,24 @@
+from qiskit.providers import Backend
+from qiskit.compiler import transpile
+from typing import Optional
+
+from quri_parts.circuit import NonParametricQuantumCircuit
+from quri_parts.circuit.transpile import CircuitTranspilerProtocol
+from quri_parts.qiskit.circuit import convert_circuit, circuit_from_qiskit
+
+
+class QiskitOptimizationTranspiler(CircuitTranspilerProtocol):
+    def __init__(self, backend: Backend, optimization_level: Optional[int] = None):
+        self._backend = backend
+        self._optimization_level = optimization_level
+
+    def __call__(
+        self, circuit: NonParametricQuantumCircuit
+    ) -> NonParametricQuantumCircuit:
+        qiskit_circ = convert_circuit(circuit)
+        optimized_qiskit_circ = transpile(
+            qiskit_circ,
+            backend=self._backend,
+            optimization_level=self._optimization_level,
+        )
+        return circuit_from_qiskit(optimized_qiskit_circ)
