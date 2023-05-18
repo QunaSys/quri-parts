eafb8665d8f711d174f084f9dd5ee9a70f7e410e
Daniel <chung@qunasys.com>
2023-05-18 16:18:49 +0800

    1. disentangle unit test 2. add license texts

--- packages/pyscf/tests/pyscf/mol/test_non_relativistic.py
+++ packages/pyscf/tests/pyscf/mol/test_non_relativistic.py
@@ -1,236 +1,203 @@
-import unittest
-from typing import cast
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#      http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-from numpy import allclose
+import unittest
+
+from numpy import allclose, array, complex128
+from numpy.typing import NDArray
 from pyscf import gto, scf
 
-from quri_parts.chem.mol import (
-    ActiveSpaceMolecularOrbitals,
-    AOeIntArraySet,
-    SpatialMOeIntSet,
-    SpinMOeIntSet,
-    cas,
-)
-from quri_parts.pyscf.mol import PySCFAOeIntSet, PySCFMolecularOrbitals, get_ao_eint_set
-
-
-def compute_integrals(
-    molecule: PySCFMolecularOrbitals, active_space_mo: ActiveSpaceMolecularOrbitals
-) -> tuple[
-    AOeIntArraySet,
+from quri_parts.chem.mol import ActiveSpaceMolecularOrbitals, cas
+from quri_parts.pyscf.mol import (
+    PySCFAO1eInt,
+    PySCFAO2eInt,
     PySCFAOeIntSet,
-    SpinMOeIntSet,
-    SpinMOeIntSet,
-    SpatialMOeIntSet,
-    SpatialMOeIntSet,
-]:
-    chem_ao_eint_set = get_ao_eint_set(molecule, store_array_on_memory=True)
-    pyscf_ao_eint_set = get_ao_eint_set(molecule)
-    pyscf_active_space_integrals = pyscf_ao_eint_set.to_active_space_mo_int(
-        active_space_mo
-    )
-    active_space_integrals = chem_ao_eint_set.to_active_space_mo_int(active_space_mo)
-    pyscf_active_space_spatial_integrals = (
-        pyscf_ao_eint_set.to_active_space_spatial_mo_int(active_space_mo)
-    )
-    active_space_spatial_integrals = chem_ao_eint_set.to_active_space_spatial_mo_int(
-        active_space_mo
-    )
-    return (
-        cast(AOeIntArraySet, chem_ao_eint_set),
-        cast(PySCFAOeIntSet, pyscf_ao_eint_set),
-        pyscf_active_space_integrals,
-        active_space_integrals,
-        pyscf_active_space_spatial_integrals,
-        active_space_spatial_integrals,
-    )
+    PySCFMolecularOrbitals,
+    PySCFSpatialMO1eInt,
+    PySCFSpatialMO2eInt,
+    get_active_space_spatial_integrals,
+)
 
 
-class ElectronIntegrals:
-    """Compare output of AOeIntArraySet and PySCFAOeIntSet."""
+class TestSpinningH3(unittest.TestCase):
+    gto_mol: gto.mole.Mole
+    mo_coeff: NDArray[complex128]
+    pyscf_mol: PySCFMolecularOrbitals
 
-    molecule: PySCFMolecularOrbitals
-    active_space_mo: ActiveSpaceMolecularOrbitals
-    chem_ao_eint_set: AOeIntArraySet
-    pyscf_ao_eint_set: PySCFAOeIntSet
-    pyscf_active_space_integrals: SpinMOeIntSet
-    active_space_integrals: SpinMOeIntSet
-    pyscf_active_space_spatial_integrals: SpatialMOeIntSet
-    active_space_spatial_integrals: SpatialMOeIntSet
-
-    def test_compare_ao1eint(self) -> None:
-        chem_ao1eint = self.chem_ao_eint_set.ao_1e_int
-        pyscf_ao1eint = self.pyscf_ao_eint_set.ao_1e_int
-        assert allclose(chem_ao1eint.array, pyscf_ao1eint.array)
-
-    def test_compare_ao2eint(self) -> None:
-        chem_ao2eint = self.chem_ao_eint_set.ao_2e_int
-        pyscf_ao2eint = self.pyscf_ao_eint_set.ao_2e_int
-        assert allclose(chem_ao2eint.array, pyscf_ao2eint.array)
-
-    def test_compare_spin_mo1eint(self) -> None:
-        chem_spin_mo1eint = self.chem_ao_eint_set.ao_1e_int.to_mo1int(
-            self.molecule.mo_coeff
-        )
-        pyscf_spin_mo1eint = self.pyscf_ao_eint_set.ao_1e_int.to_mo1int(
-            self.molecule.mo_coeff
-        )
-        assert allclose(chem_spin_mo1eint.array, pyscf_spin_mo1eint.array)
-
-    def test_compare_spin_mo2eint(self) -> None:
-        chem_spin_mo2eint = self.chem_ao_eint_set.ao_2e_int.to_mo2int(
-            self.molecule.mo_coeff
-        )
-        pyscf_spin_mo2eint = self.pyscf_ao_eint_set.ao_2e_int.to_mo2int(
-            self.molecule.mo_coeff
-        )
-        assert allclose(chem_spin_mo2eint.array, pyscf_spin_mo2eint.array)
-
-    def test_compare_spatial_mo1eint(self) -> None:
-        chem_spatial_mo1eint = self.chem_ao_eint_set.ao_1e_int.to_spatial_mo1int(
-            self.molecule.mo_coeff
-        )
-        pyscf_spatial_mo1eint = self.pyscf_ao_eint_set.ao_1e_int.to_spatial_mo1int(
-            self.molecule.mo_coeff
-        )
-        assert allclose(chem_spatial_mo1eint.array, pyscf_spatial_mo1eint.array)
-
-    def test_compare_spatial_mo2eint(self) -> None:
-        chem_spatial_mo2eint = self.chem_ao_eint_set.ao_2e_int.to_spatial_mo2int(
-            self.molecule.mo_coeff
-        )
-        pyscf_spatial_mo2eint = self.pyscf_ao_eint_set.ao_2e_int.to_spatial_mo2int(
-            self.molecule.mo_coeff
-        )
-        assert allclose(chem_spatial_mo2eint.array, pyscf_spatial_mo2eint.array)
-
-    def test_compare_casci_const(self) -> None:
-        assert allclose(
-            self.pyscf_active_space_integrals.const, self.active_space_integrals.const
-        )
-
-    def test_compare_casci_spin_mo_1eint(self) -> None:
-        assert allclose(
-            self.pyscf_active_space_integrals.mo_1e_int.array,
-            self.active_space_integrals.mo_1e_int.array,
-        )
-
-    def test_compare_casci_spin_mo_2eint(self) -> None:
-        assert allclose(
-            self.pyscf_active_space_integrals.mo_2e_int.array,
-            self.active_space_integrals.mo_2e_int.array,
-        )
-
-    def test_compare_casci_const_from_spatial(self) -> None:
-        assert allclose(
-            self.pyscf_active_space_spatial_integrals.const,
-            self.active_space_spatial_integrals.const,
-        )
-
-    def test_compare_casci_spatial_mo_1eint(self) -> None:
-        assert allclose(
-            self.pyscf_active_space_spatial_integrals.mo_1e_int.array,
-            self.active_space_spatial_integrals.mo_1e_int.array,
-        )
-
-    def test_compare_casci_spatial_mo_2eint(self) -> None:
-        assert allclose(
-            self.pyscf_active_space_spatial_integrals.mo_2e_int.array,
-            self.active_space_spatial_integrals.mo_2e_int.array,
-        )
-
-
-class TestH2O(ElectronIntegrals, unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
-        atom_list = [["O", [0, 0, 0]], ["H", [0, 0, 1]], ["H", [2, 0, 0.5]]]
-        mol = gto.M(atom=atom_list)
-        mf = scf.RHF(mol)
+        atoms = [["H", [0, 0, i]] for i in range(3)]
+        cls.gto_mol = gto.M(atom=atoms, spin=1)
+        mf = scf.RHF(cls.gto_mol)
         mf.kernel()
 
-        cls.molecule = PySCFMolecularOrbitals(mol=mol, mo_coeff=mf.mo_coeff)
-        cls.active_space_mo = ActiveSpaceMolecularOrbitals(
-            cls.molecule, cas(n_active_ele=6, n_active_orb=4)
+        cls.mo_coeff = mf.mo_coeff
+        cls.pyscf_mol = PySCFMolecularOrbitals(cls.gto_mol, cls.mo_coeff)
+
+    def test_ao_1e_int(self) -> None:
+        pyscf_ao_1e_int = PySCFAO1eInt(self.gto_mol)
+        expected_ao_1e_int = array(
+            [
+                [-1.24398564, -0.86080698, -0.22631877],
+                [-0.86080698, -1.4924109, -0.86080698],
+                [-0.22631877, -0.86080698, -1.24398564],
+            ]
         )
-        (
-            cls.chem_ao_eint_set,
-            cls.pyscf_ao_eint_set,
-            cls.pyscf_active_space_integrals,
-            cls.active_space_integrals,
-            cls.pyscf_active_space_spatial_integrals,
-            cls.active_space_spatial_integrals,
-        ) = compute_integrals(
-            molecule=cls.molecule, active_space_mo=cls.active_space_mo
+        assert allclose(pyscf_ao_1e_int.array, expected_ao_1e_int)
+
+    def test_spatial_mo_1e_int(self) -> None:
+        pyscf_spatial_mo_1e_int = PySCFSpatialMO1eInt(self.gto_mol, self.mo_coeff)
+        expected_spatial_mo_1e_int = array(
+            [
+                [-1.53466912, -0.0, -0.12053411],
+                [0.0, -1.15541096, -0.0],
+                [-0.12053411, -0.0, -0.75066168],
+            ]
         )
+        assert allclose(pyscf_spatial_mo_1e_int.array, expected_spatial_mo_1e_int)
 
-
-class TestSpinningH2O(ElectronIntegrals, unittest.TestCase):
-    @classmethod
-    def setUpClass(cls) -> None:
-        atom_list = [["O", [0, 0, 0]], ["H", [0, 0, 1]], ["H", [2, 0, 0.5]]]
-        mol = gto.M(atom=atom_list, spin=2)
-        mf = scf.ROHF(mol)
-        mf.kernel()
-
-        cls.molecule = PySCFMolecularOrbitals(mol=mol, mo_coeff=mf.mo_coeff)
-        cls.active_space_mo = ActiveSpaceMolecularOrbitals(
-            cls.molecule, cas(n_active_ele=6, n_active_orb=4)
+    def test_ao_2e_int(self) -> None:
+        pyscf_ao_2e_int = PySCFAO2eInt(self.gto_mol)
+        expected_ao_2e_int = array(
+            [
+                [
+                    [
+                        [0.77460594, 0.30930897, 0.05584872],
+                        [0.30930897, 0.15786578, 0.03152398],
+                        [0.05584872, 0.03152398, 0.00732843],
+                    ],
+                    [
+                        [0.30930897, 0.15786578, 0.03152398],
+                        [0.47804137, 0.30930897, 0.07065588],
+                        [0.17348403, 0.11521591, 0.03152398],
+                    ],
+                    [
+                        [0.05584872, 0.03152398, 0.00732843],
+                        [0.17348403, 0.11521591, 0.03152398],
+                        [0.26369505, 0.17348403, 0.05584872],
+                    ],
+                ],
+                [
+                    [
+                        [0.30930897, 0.47804137, 0.17348403],
+                        [0.15786578, 0.30930897, 0.11521591],
+                        [0.03152398, 0.07065588, 0.03152398],
+                    ],
+                    [
+                        [0.15786578, 0.30930897, 0.11521591],
+                        [0.30930897, 0.77460594, 0.30930897],
+                        [0.11521591, 0.30930897, 0.15786578],
+                    ],
+                    [
+                        [0.03152398, 0.07065588, 0.03152398],
+                        [0.11521591, 0.30930897, 0.15786578],
+                        [0.17348403, 0.47804137, 0.30930897],
+                    ],
+                ],
+                [
+                    [
+                        [0.05584872, 0.17348403, 0.26369505],
+                        [0.03152398, 0.11521591, 0.17348403],
+                        [0.00732843, 0.03152398, 0.05584872],
+                    ],
+                    [
+                        [0.03152398, 0.11521591, 0.17348403],
+                        [0.07065588, 0.30930897, 0.47804137],
+                        [0.03152398, 0.15786578, 0.30930897],
+                    ],
+                    [
+                        [0.00732843, 0.03152398, 0.05584872],
+                        [0.03152398, 0.15786578, 0.30930897],
+                        [0.05584872, 0.30930897, 0.77460594],
+                    ],
+                ],
+            ]
         )
-        (
-            cls.chem_ao_eint_set,
-            cls.pyscf_ao_eint_set,
-            cls.pyscf_active_space_integrals,
-            cls.active_space_integrals,
-            cls.pyscf_active_space_spatial_integrals,
-            cls.active_space_spatial_integrals,
-        ) = compute_integrals(
-            molecule=cls.molecule, active_space_mo=cls.active_space_mo
+        assert allclose(pyscf_ao_2e_int.array, expected_ao_2e_int)
+
+    def test_spatial_mo_2e_int(self) -> None:
+        pyscf_spatial_mo_2e_int = PySCFSpatialMO2eInt(self.gto_mol, self.mo_coeff)
+        expected_spatial_mo_2e_int = array(
+            [
+                [
+                    [
+                        [0.55936827, -0.0, 0.08803043],
+                        [-0.0, 0.15477155, 0.0],
+                        [0.08803043, 0.0, 0.13563321],
+                    ],
+                    [
+                        [-0.0, 0.15477155, 0.0],
+                        [0.48660662, -0.0, -0.0379191],
+                        [0.0, -0.14084555, -0.0],
+                    ],
+                    [
+                        [0.08803043, 0.0, 0.13563321],
+                        [0.0, -0.14084555, -0.0],
+                        [0.57575727, -0.0, 0.08987634],
+                    ],
+                ],
+                [
+                    [
+                        [-0.0, 0.48660662, 0.0],
+                        [0.15477155, -0.0, -0.14084555],
+                        [0.0, -0.0379191, -0.0],
+                    ],
+                    [
+                        [0.15477155, -0.0, -0.14084555],
+                        [-0.0, 0.53466412, 0.0],
+                        [-0.14084555, -0.0, 0.15039551],
+                    ],
+                    [
+                        [0.0, -0.0379191, -0.0],
+                        [-0.14084555, -0.0, 0.15039551],
+                        [-0.0, 0.5123315, 0.0],
+                    ],
+                ],
+                [
+                    [
+                        [0.08803043, 0.0, 0.57575727],
+                        [0.0, -0.14084555, -0.0],
+                        [0.13563321, -0.0, 0.08987634],
+                    ],
+                    [
+                        [0.0, -0.14084555, -0.0],
+                        [-0.0379191, 0.0, 0.5123315],
+                        [-0.0, 0.15039551, 0.0],
+                    ],
+                    [
+                        [0.13563321, -0.0, 0.08987634],
+                        [-0.0, 0.15039551, 0.0],
+                        [0.08987634, 0.0, 0.62546613],
+                    ],
+                ],
+            ]
         )
+        assert allclose(pyscf_spatial_mo_2e_int.array, expected_spatial_mo_2e_int)
 
+    def test_active_space_spatial_integrals(self) -> None:
+        asmo = ActiveSpaceMolecularOrbitals(self.pyscf_mol, cas(1, 1))
+        nuc_energy = 1.3229430273
+        pyscf_ao_1e_int = PySCFAO1eInt(self.gto_mol)
+        pyscf_ao_2e_int = PySCFAO2eInt(self.gto_mol)
 
-class TestCCPVDZH2O(ElectronIntegrals, unittest.TestCase):
-    @classmethod
-    def setUpClass(cls) -> None:
-        atom_list = [["O", [0, 0, 0]], ["H", [0, 0, 1]], ["H", [2, 0, 0.5]]]
-        mol = gto.M(atom=atom_list, basis="ccpvdz")
-        mf = scf.RHF(mol)
-        mf.kernel()
-
-        cls.molecule = PySCFMolecularOrbitals(mol=mol, mo_coeff=mf.mo_coeff)
-        cls.active_space_mo = ActiveSpaceMolecularOrbitals(
-            cls.molecule, cas(n_active_ele=6, n_active_orb=4)
+        electron_ints = PySCFAOeIntSet(
+            mol=self.gto_mol,
+            constant=nuc_energy,
+            ao_1e_int=pyscf_ao_1e_int,
+            ao_2e_int=pyscf_ao_2e_int,
         )
-        (
-            cls.chem_ao_eint_set,
-            cls.pyscf_ao_eint_set,
-            cls.pyscf_active_space_integrals,
-            cls.active_space_integrals,
-            cls.pyscf_active_space_spatial_integrals,
-            cls.active_space_spatial_integrals,
-        ) = compute_integrals(
-            molecule=cls.molecule, active_space_mo=cls.active_space_mo
-        )
+        spatial_as_mo_eint_set = get_active_space_spatial_integrals(asmo, electron_ints)
+        as_mo_1e_int = spatial_as_mo_eint_set.mo_1e_int.array
+        expected_mo_1e_int = array([[-0.33696926]])
+        as_mo_2e_int = spatial_as_mo_eint_set.mo_2e_int.array
+        expected_mo_2e_int = array([[[[0.53466412]]]])
 
-
-class TestChargedH2O(ElectronIntegrals, unittest.TestCase):
-    @classmethod
-    def setUpClass(cls) -> None:
-        atom_list = [["O", [0, 0, 0]], ["H", [0, 0, 1]], ["H", [2, 0, 0.5]]]
-        mol = gto.M(atom=atom_list, spin=1, charge=1)
-        mf = scf.ROHF(mol)
-        mf.kernel()
-
-        cls.molecule = PySCFMolecularOrbitals(mol=mol, mo_coeff=mf.mo_coeff)
-        cls.active_space_mo = ActiveSpaceMolecularOrbitals(
-            cls.molecule, cas(n_active_ele=5, n_active_orb=4)
-        )
-        (
-            cls.chem_ao_eint_set,
-            cls.pyscf_ao_eint_set,
-            cls.pyscf_active_space_integrals,
-            cls.active_space_integrals,
-            cls.pyscf_active_space_spatial_integrals,
-            cls.active_space_spatial_integrals,
-        ) = compute_integrals(
-            molecule=cls.molecule, active_space_mo=cls.active_space_mo
-        )
+        assert allclose(as_mo_1e_int, expected_mo_1e_int)
+        assert allclose(as_mo_2e_int, expected_mo_2e_int)
