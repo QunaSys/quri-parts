1bf6b025f0df810402480b5df3dc88839dd90cc7
Mr.WakuWaku <mr.wakwak8989@gmail.com>
2023-05-22 14:54:17 +0900

    Re-export PostSelectionFilterFunction

--- packages/algo/quri_parts/algo/mitigation/post_selection/__init__.py
+++ packages/algo/quri_parts/algo/mitigation/post_selection/__init__.py
@@ -1,4 +1,5 @@
 from .post_selection import (
+    PostSelectionFilterFunction,
     create_bk_electron_number_post_selection_sampler,
     create_general_post_selection_sampler,
     create_jw_electron_number_post_selection_sampler,
@@ -8,4 +9,5 @@ __all__ = [
     "create_bk_electron_number_post_selection_sampler",
     "create_general_post_selection_sampler",
     "create_jw_electron_number_post_selection_sampler",
+    "PostSelectionFilterFunction",
 ]
