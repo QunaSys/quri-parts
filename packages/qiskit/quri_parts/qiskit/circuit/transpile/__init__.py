bea09061de6c5835bca25540e6ed3a73de9248c7
Kentaro Wada <wada.k@qunasys.com>
2023-05-23 14:44:15 +0900

    add docs for qiskit trans

--- packages/qiskit/quri_parts/qiskit/circuit/transpile/__init__.py
+++ packages/qiskit/quri_parts/qiskit/circuit/transpile/__init__.py
@@ -9,6 +9,21 @@ from quri_parts.qiskit.circuit import circuit_from_qiskit, convert_circuit
 
 
 class QiskitOptimizationTranspiler(CircuitTranspilerProtocol):
+    """A CircuitTranspiler that uses Qiskit's transpiler to convert circuits to
+    backend-compatible circuits, convert gate sets, perform circuit
+    optimization, etc.
+
+    This transpiler converts NonParametricQuantumCircuit to NonParametricQuantumCircuit
+    just like other transpilers in QURI Parts though the conversion of the circuit to
+    Qiskit and vice versa is performed internally.
+
+    Args:
+        backend: Qiskit's Backend instance.
+        basis_gates: Specify the gate set after decomposition as a string of gates.
+            The gate name notation follows Qiskit.
+        optimization_level: Specifies the optimization level of the circuit.
+    """
+
     def __init__(
         self,
         backend: Optional[Backend] = None,
