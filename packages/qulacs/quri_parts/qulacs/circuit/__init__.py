d833a0448e1e26071aa53bc663c76e0dc70825d8
Kentaro Wada <wada.k@qunasys.com>
2023-06-07 18:14:07 +0900

    cleanup code

--- packages/qulacs/quri_parts/qulacs/circuit/__init__.py
+++ packages/qulacs/quri_parts/qulacs/circuit/__init__.py
@@ -11,7 +11,6 @@
 from collections.abc import Mapping, Sequence
 from typing import Callable, Union, cast
 
-import numpy as np
 import qulacs
 from numpy.typing import ArrayLike
 from typing_extensions import assert_never
