a91ce9041f330ae4aec143e4cbc7787b70b8a043
Kentaro Wada <wada.k@qunasys.com>
2023-06-14 16:06:00 +0900

    fix comment

--- packages/circuit/tests/circuit/transpile/test_gate_kind_decomposer.py
+++ packages/circuit/tests/circuit/transpile/test_gate_kind_decomposer.py
@@ -461,8 +461,8 @@ class TestRZSetTranspile:
                 CNOT(1, 2),
                 RZ(2, -np.pi / 4.0),  # Tdag
                 CNOT(0, 2),
-                RZ(1, np.pi / 4.0),  # T, H
-                RZ(2, np.pi * 3.0 / 4.0),
+                RZ(1, np.pi / 4.0),  # T
+                RZ(2, np.pi * 3.0 / 4.0),  # T, H
                 SqrtX(2),
                 RZ(2, np.pi / 2.0),
                 CNOT(0, 1),
