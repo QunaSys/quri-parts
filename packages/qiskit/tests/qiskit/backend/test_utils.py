d1a7d429f4a58f1f72203c28c85ed3c70f2d5f09
Daniel <chung@qunasys.com>
2023-06-19 14:24:00 +0800

    split test cases in test_utils.py

--- packages/qiskit/tests/qiskit/backend/test_utils.py
+++ packages/qiskit/tests/qiskit/backend/test_utils.py
@@ -21,32 +21,42 @@ from quri_parts.qiskit.backend import (
 )
 
 
-def circuit_transpiler(_: NonParametricQuantumCircuit) -> NonParametricQuantumCircuit:
-    circuit = QuantumCircuit(2)
-    circuit.add_X_gate(0)
-    circuit.add_X_gate(0)
-    circuit.add_Y_gate(1)
-    circuit.add_Y_gate(1)
-    return circuit
+class TestDistributeBackendShots:
+    def test_min_shot_1_disable_shots_roundup(self) -> None:
+        shot_distribution_0 = distribute_backend_shots(
+            1080, 1, 100, enable_shots_roundup=False
+        )
+        assert shot_distribution_0 == [100] * 10 + [80]
 
+    def test_min_shot_1_enable_shots_roundup(self) -> None:
+        shot_distribution = distribute_backend_shots(
+            1080, 1, 100, enable_shots_roundup=True
+        )
+        assert shot_distribution == [100] * 10 + [80]
 
-def test_distribute_backend_shots() -> None:
-    shot_distribution_0 = distribute_backend_shots(
-        1080, 1, 100, enable_shots_roundup=False
-    )
-    assert shot_distribution_0 == [100] * 10 + [80]
-    shot_distribution_1 = distribute_backend_shots(
-        1080, 1, 100, enable_shots_roundup=True
-    )
-    assert shot_distribution_1 == [100] * 10 + [80]
-    shot_distribution_2 = distribute_backend_shots(
-        1020, 30, 100, enable_shots_roundup=False
-    )
-    assert shot_distribution_2 == [100] * 10
-    shot_distribution_3 = distribute_backend_shots(
-        1020, 30, 100, enable_shots_roundup=True
-    )
-    assert shot_distribution_3 == [100] * 10 + [30]
+    def test_min_shot_30_disable_shots_roundup(self) -> None:
+        shot_distribution = distribute_backend_shots(
+            1020, 30, 100, enable_shots_roundup=False
+        )
+        assert shot_distribution == [100] * 10
+
+    def test_min_shot_30_enable_shots_roundup(self) -> None:
+        shot_distribution = distribute_backend_shots(
+            1020, 30, 100, enable_shots_roundup=True
+        )
+        assert shot_distribution == [100] * 10 + [30]
+
+    def test_shot_split_evenly_disable_shots_roundup(self) -> None:
+        shot_distribution = distribute_backend_shots(
+            200, 1, 100, enable_shots_roundup=False
+        )
+        assert shot_distribution == [100, 100]
+
+    def test_shot_split_evenly_enable_shots_roundup(self) -> None:
+        shot_distribution = distribute_backend_shots(
+            200, 1, 100, enable_shots_roundup=True
+        )
+        assert shot_distribution == [100, 100]
 
 
 def test_get_backend_min_max_shot() -> None:
@@ -56,7 +66,7 @@ def test_get_backend_min_max_shot() -> None:
     assert max_shot == int(1e6)
 
 
-def test_get_job_mapper_and_circuit_transpiler() -> None:
+class TestGetJobMapperAndCircuitTranspiler:
     raw_data = {"00": 1, "01": 2, "10": 3, "11": 4}
     saved_result = QiskitSavedDataSamplingResult(raw_data=raw_data)
     saved_job = QiskitSavedDataSamplingJob(
@@ -64,41 +74,56 @@ def test_get_job_mapper_and_circuit_transpiler() -> None:
     )
     circuit = QuantumCircuit(2)
 
-    # qubit_mapping is None, circuit_transpiler is None
-    job_mapper_0, circuit_transpiler_0 = get_job_mapper_and_circuit_transpiler()
-    assert job_mapper_0(saved_job).result().counts == {0: 1, 1: 2, 2: 3, 3: 4}
-    assert circuit_transpiler_0(circuit) == circuit
+    @staticmethod
+    def circuit_transpiler(
+        _: NonParametricQuantumCircuit,
+    ) -> NonParametricQuantumCircuit:
+        circuit = QuantumCircuit(2)
+        circuit.add_X_gate(0)
+        circuit.add_X_gate(0)
+        circuit.add_Y_gate(1)
+        circuit.add_Y_gate(1)
+        return circuit
 
-    # qubit_mapping is not None, circuit_transpiler is None
-    job_mapper_1, circuit_transpiler_1 = get_job_mapper_and_circuit_transpiler(
-        qubit_mapping={0: 1, 1: 0}
-    )
-    assert job_mapper_1(saved_job).result().counts == {0: 1, 2: 2, 1: 3, 3: 4}
-    assert circuit_transpiler_1(circuit) == circuit
+    def test_no_mapper_and_no_transpiler(self) -> None:
+        # qubit_mapping is None, circuit_transpiler is None
+        job_mapper, circuit_transpiler = get_job_mapper_and_circuit_transpiler()
+        assert job_mapper(self.saved_job).result().counts == {0: 1, 1: 2, 2: 3, 3: 4}
+        assert circuit_transpiler(self.circuit) == self.circuit
 
-    # qubit_mapping is None, circuit_transpiler is not None
-    job_mapper_2, circuit_transpiler_2 = get_job_mapper_and_circuit_transpiler(
-        circuit_transpiler=circuit_transpiler
-    )
-    assert job_mapper_2(saved_job).result().counts == {0: 1, 1: 2, 2: 3, 3: 4}
-    expected_transpiled_circuit = QuantumCircuit(2)
-    expected_transpiled_circuit.add_X_gate(0)
-    expected_transpiled_circuit.add_X_gate(0)
-    expected_transpiled_circuit.add_Y_gate(1)
-    expected_transpiled_circuit.add_Y_gate(1)
-    assert circuit_transpiler_2(circuit) == expected_transpiled_circuit
+    def test_non_trivial_mapper_and_no_transpiler(self) -> None:
+        # qubit_mapping is not None, circuit_transpiler is None
+        job_mapper, circuit_transpiler = get_job_mapper_and_circuit_transpiler(
+            qubit_mapping={0: 1, 1: 0}
+        )
+        assert job_mapper(self.saved_job).result().counts == {0: 1, 2: 2, 1: 3, 3: 4}
+        assert circuit_transpiler(self.circuit) == self.circuit
 
-    # qubit_mapping is not None, circuit_transpiler is not None
-    job_mapper_3, circuit_transpiler_3 = get_job_mapper_and_circuit_transpiler(
-        qubit_mapping={0: 1, 1: 0}, circuit_transpiler=circuit_transpiler
-    )
-    assert job_mapper_3(saved_job).result().counts == {0: 1, 2: 2, 1: 3, 3: 4}
-    expected_transpiled_circuit_2 = QuantumCircuit(2)
-    expected_transpiled_circuit_2.add_X_gate(1)
-    expected_transpiled_circuit_2.add_X_gate(1)
-    expected_transpiled_circuit_2.add_Y_gate(0)
-    expected_transpiled_circuit_2.add_Y_gate(0)
-    assert circuit_transpiler_3(circuit) == expected_transpiled_circuit_2
+    def test_no_mapper_and_non_trivial_transpiler(self) -> None:
+        # qubit_mapping is None, circuit_transpiler is not None
+        job_mapper, circuit_transpiler = get_job_mapper_and_circuit_transpiler(
+            circuit_transpiler=self.circuit_transpiler
+        )
+        assert job_mapper(self.saved_job).result().counts == {0: 1, 1: 2, 2: 3, 3: 4}
+        expected_transpiled_circuit = QuantumCircuit(2)
+        expected_transpiled_circuit.add_X_gate(0)
+        expected_transpiled_circuit.add_X_gate(0)
+        expected_transpiled_circuit.add_Y_gate(1)
+        expected_transpiled_circuit.add_Y_gate(1)
+        assert circuit_transpiler(self.circuit) == expected_transpiled_circuit
+
+    def test_non_trivial_mapper_and_non_trivial_transpiler(self) -> None:
+        # qubit_mapping is not None, circuit_transpiler is not None
+        job_mapper, circuit_transpiler = get_job_mapper_and_circuit_transpiler(
+            qubit_mapping={0: 1, 1: 0}, circuit_transpiler=self.circuit_transpiler
+        )
+        assert job_mapper(self.saved_job).result().counts == {0: 1, 2: 2, 1: 3, 3: 4}
+        expected_transpiled_circuit = QuantumCircuit(2)
+        expected_transpiled_circuit.add_X_gate(1)
+        expected_transpiled_circuit.add_X_gate(1)
+        expected_transpiled_circuit.add_Y_gate(0)
+        expected_transpiled_circuit.add_Y_gate(0)
+        assert circuit_transpiler(self.circuit) == expected_transpiled_circuit
 
 
 def test_convert_qiskit_sampling_count_to_qp_sampling_count() -> None:
