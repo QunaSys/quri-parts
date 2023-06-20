5b898fe7606d2e76cdae5be05547a24a4b8570be
Toru Kawakubo <kawakubo@qunasys.com>
2023-06-20 19:47:48 +0900

    Add a test for recording tested function invocations

--- packages/core/tests/core/utils/test_recording.py
+++ packages/core/tests/core/utils/test_recording.py
@@ -39,3 +39,49 @@ def test_recordable() -> None:
         RecordEntry(fid, ("x", 4)),
         RecordEntry(fid, ("2x", 8)),
     ]
+
+
+@recordable
+def nested_func(recorder: Recorder, x: int) -> int:
+    recorder.info("x", x)
+    if x // 2 == 1:
+        y = nested_func(2 * x)
+        recorder.info("y", y)
+    else:
+        y = x
+    recorder.info("2y", 2 * y)
+    return 2 * y
+
+
+def test_nested_func_record() -> None:
+    fid = nested_func.id
+    session = RecordSession()
+    session.set_level(INFO, nested_func)
+
+    with session.start():
+        assert nested_func(3) == 24
+        assert nested_func(4) == 8
+
+    records = session.get_records()
+    history = list(records.get_history(nested_func))
+    assert len(history) == 3
+    group0, group1, group2 = history
+
+    assert group0.func_id == fid
+    assert group0.entries == [
+        RecordEntry(fid, ("x", 3)),
+        RecordEntry(fid, ("y", 12)),
+        RecordEntry(fid, ("2y", 24)),
+    ]
+
+    assert group1.func_id == fid
+    assert group1.entries == [
+        RecordEntry(fid, ("x", 6)),
+        RecordEntry(fid, ("2y", 12)),
+    ]
+
+    assert group2.func_id == fid
+    assert group2.entries == [
+        RecordEntry(fid, ("x", 4)),
+        RecordEntry(fid, ("2y", 8)),
+    ]
