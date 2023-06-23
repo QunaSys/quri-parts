2e87d17db9280d6790aa41d1e618767a14e462fb
Kentaro Wada <wada.k@qunasys.com>
2023-06-23 13:01:56 +0900

    wip: cleanup code, fix type annotations

--- packages/qiskit/quri_parts/qiskit/backend/connectivity.py
+++ packages/qiskit/quri_parts/qiskit/backend/connectivity.py
@@ -1,10 +1,8 @@
-import math
 from typing import Any, Optional, Union
 
 import networkx as nx
 import numpy as np
 from qiskit.providers import BackendV1, BackendV2
-from z3 import And, Bool, If, Implies, Int, IntVector, Optimize, Or, sat
 
 
 def device_connectivity_graph(device: Union[BackendV1, BackendV2]) -> nx.Graph:
@@ -71,7 +69,7 @@ def qubit_counts_considering_cx_errors(
     return [len(c) for c in nx.connected_components(graph)]
 
 
-def _undirected(g):
+def _undirected(g: dict[tuple[int, int], float]) -> dict[tuple[int, int], float]:
     rs = {}
     for (a, b), e in g.items():
         if (a, b) not in rs and (b, a) not in rs:
@@ -79,7 +77,7 @@ def _undirected(g):
     return rs
 
 
-def _directed(g):
+def _directed(g: dict[tuple[int, int], float]) -> dict[tuple[int, int], float]:
     rs = {}
     for (a, b), e in g.items():
         rs[(b, a)] = e
@@ -98,7 +96,7 @@ def approx_cx_reliable_subgraph(
 ) -> list[nx.Graph]:
     sorted_graph = sorted(_undirected(cx_errors).items(), key=lambda x: x[1])
     for i in range(qubits - 1, len(sorted_graph)):
-        sg = _list_to_graph(_directed({k: v for k, v in sorted_graph[:i]}).keys())
+        sg = _list_to_graph(list(_directed({k: v for k, v in sorted_graph[:i]}).keys()))
         rs = [sg.subgraph(c) for c in nx.connected_components(sg) if len(c) >= qubits]
         if rs:
             return rs
@@ -111,7 +109,7 @@ def _length_satisfactory_paths(graph: nx.Graph, qubits: int) -> list[list[str]]:
     for s in nodes:
         for e in nodes:
             ps = nx.all_simple_paths(graph, s, e)
-            ret.extend([p for p in ps if len(p) >= qubits])
+            ret.extend([p for p in ps if len(p) == qubits])
     return ret
 
 
@@ -120,7 +118,7 @@ def _approx_cx_reliable_single_stroke_paths(
 ) -> list[list[str]]:
     sorted_graph = sorted(_undirected(cx_errors).items(), key=lambda x: x[1])
     for i in range(qubits - 1, len(sorted_graph)):
-        sg = _list_to_graph(_directed({k: v for k, v in sorted_graph[:i]}).keys())
+        sg = _list_to_graph(list(_directed({k: v for k, v in sorted_graph[:i]}).keys()))
         rs = [sg.subgraph(c) for c in nx.connected_components(sg) if len(c) >= qubits]
         print(f"{i}: {[len(c) for c in nx.connected_components(sg)]}")
         ret = []
@@ -139,10 +137,10 @@ def _path_fidelity(cx_errors: dict[tuple[int, int], float], path: list[int]) -> 
 def cx_reliable_single_stroke_path(
     cx_errors: dict[tuple[int, int], float],
     qubits: int,
-    exact: Bool = True,
+    exact: bool = True,
 ) -> list[int]:
     if exact:
-        ps = _length_satisfactory_paths(_list_to_graph(cx_errors.keys()), qubits)
+        ps = _length_satisfactory_paths(_list_to_graph(list(cx_errors.keys())), qubits)
     else:
         ps = _approx_cx_reliable_single_stroke_paths(cx_errors, qubits)
     ret = []
@@ -154,86 +152,3 @@ def cx_reliable_single_stroke_path(
             fc = f
             ret = path
     return ret
-
-
-def optimized_single_stroke_subgraph(
-    graph: dict[tuple[int, int], float], qubits: int
-) -> Optional[nx.Graph]:
-    s = Optimize()
-
-    n = len(set(sum(graph.keys(), ())))
-    edge_count = qubits - 1
-
-    adj_mat = [[Int("adj[%d,%d]" % (i, j)) for j in range(n)] for i in range(n)]
-    cost = 0.0
-    for i in range(n):
-        for j in range(n):
-            s.add(Implies(adj_mat[i][j] == 1, (i, j) in graph))
-            s.add(If(adj_mat[i][j] == 1, adj_mat[j][i] == 1, adj_mat[j][i] == 0))
-            s.add(Or(adj_mat[i][j] == 1, adj_mat[i][j] == 0))
-            cost = cost + If(adj_mat[i][j] == 1, graph.get((i, j), 0.0), 0.0)
-
-    deg1 = IntVector("deg1", n)
-    deg2 = IntVector("deg2", n)
-    for i in range(n):
-        s.add(sum(adj_mat[i]) <= 2)
-        s.add(If(sum(adj_mat[i]) == 1, deg1[i] == 1, deg1[i] == 0))
-        s.add(If(sum(adj_mat[i]) == 2, deg2[i] == 1, deg2[i] == 0))
-    s.add(sum(deg1) == 2)
-    s.add(sum(deg2) == edge_count - 1)
-
-    path = [[Bool("path[%d,%d]" % (i, j)) for j in range(n)] for i in range(n)]
-    for i in range(n):
-        for j in range(n):
-            path[i][j] = False
-        path[i][i] = True
-    for k in range(n):
-        for i in range(n):
-            for j in range(n):
-                path[i][j] = Or(
-                    path[j][i],
-                    path[i][j],
-                    And(path[i][k], Or(adj_mat[k][j] == 1, adj_mat[j][k] == 1)),
-                )
-    for i in range(n):
-        for j in range(n):
-            s.add(
-                Implies(
-                    And(
-                        Or(deg1[i] == 1, deg2[i] == 1),
-                        Or(deg1[j] == 1, deg2[j] == 1),
-                    ),
-                    path[i][j],
-                )
-            )
-
-    s.minimize(cost)
-
-    r = s.check()
-    if r != sat:
-        # raise RuntimeError("No subgraphs were found by SAT that met the criteria.")
-        return None
-    m = s.model()
-    adj_list = []
-    for i in range(n):
-        for j in range(n):
-            if m[adj_mat[i][j]] == 1:
-                adj_list.append(f"{i} {j}")
-    return nx.parse_adjlist(adj_list)
-
-
-def optimized_single_stroke_path(
-    device: BackendV2, qubits: int
-) -> Optional[dict[tuple[int, int]]]:
-    cx_errors = coupling_map_with_cx_errors(device)
-
-    cx_lnfidelity = {}
-    for q, e in cx_errors.items():
-        cx_lnfidelity[q] = -math.log(1.0 - e)
-
-    graph = optimized_single_stroke_subgraph(cx_lnfidelity, qubits)
-    if graph is None:
-        return None
-
-    path = [(int(a), int(b)) for a, b in nx.eulerian_path(graph)]
-    return {q: cx_errors[q] for q in path}
