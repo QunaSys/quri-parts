ceaf322225d9e4d36f78326c73a21800a3a0df4f
Daniel <chung@qunasys.com>
2023-06-26 17:36:42 +0800

    add comments for the compile functions

--- packages/qulacs/quri_parts/qulacs/circuit/compiled_circuit.py
+++ packages/qulacs/quri_parts/qulacs/circuit/compiled_circuit.py
@@ -18,6 +18,21 @@ from quri_parts.circuit import (
 
 
 def compile_circuit(circuit: NonParametricQuantumCircuit) -> "_QulacsCircuit":
+    """Compiles a quri-parts circuit into an `ImmutableQuantumCircuit` that
+    holds the corresponding qulacs circuit on memory. The qulacs circuit can be
+    accessed by the `.qulacs_circuit` property.
+
+    Example:
+
+    >>> circuit = QuantumCircuit(2)
+    >>> circuit.add_X_gate(0)
+    >>> circuit.add_X_gate(1)
+
+    Get compiled circuit
+
+    >>> compiled_circuit = compile_circuit(circuit)
+    >>> compiled_qulacs_circuit = compiled_circuit.qulacs_circuit
+    """
     return _QulacsCircuit(circuit)
 
 
@@ -42,6 +57,37 @@ def compile_parametric_circuit(
 ) -> Union[
     "_QulacsUnboundParametricCircuit", "_QulacsLinearMappedUnboundParametricCircuit"
 ]:
+    """Compiles a:
+
+        - quri-parts `UnboundParametricQuantumCircuit` into an
+            `ImmutableUnboundParametricQuantumCircuit` that holds the corresponding
+            qulacs parametric circuit on memory.
+        - quri-parts `LinearMappedUnboundParametricQuantumCircuit` into an
+            `ImmutableUnboundParametricQuantumCircuit` that holds the corresponding
+            qulacs parametric circuit on memory.
+
+    The qulacs circuit can be accessed by the `.qulacs_circuit` property.
+
+    The parameter mapper that maps the quri-parts circuit parameters to qulacs
+    parametric circuit parameters can be accessed by the `.param_mapper` property.
+
+    Example:
+
+    >>> parametric_circuit = UnboundParametricQuantumCircuit(2)
+    >>> parametric_circuit.add_ParametricRX_gate(0)
+    >>> parametric_circuit.add_ParametricRX_gate(1)
+
+    1. Get compiled parametric circuit
+
+    >>> compiled_parametric_circuit = compile_circuit(circuit)
+    >>> compiled_qulacs_circuit = compiled_circuit.qulacs_circuit
+
+    2. Get qulacs circuit parameters
+
+    >>> param_mapper = compiled_circuit.param_mapper
+    >>> quri_parts_circuit_param = [0, 1]
+    >>> qulacs_circuit_param = param_mapper(quri_parts_circuit_param)
+    """
     if isinstance(circuit, UnboundParametricQuantumCircuit):
         return _QulacsUnboundParametricCircuit(circuit)
     elif isinstance(circuit, LinearMappedUnboundParametricQuantumCircuit):
