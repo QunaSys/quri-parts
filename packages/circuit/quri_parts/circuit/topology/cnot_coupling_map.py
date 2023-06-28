e673c4c9590f18befcffb6b7b3cb233a25a6c87a
Kentaro Wada <wada.k@qunasys.com>
2023-06-28 12:01:37 +0900

    fix graph to list converter

--- packages/circuit/quri_parts/circuit/topology/cnot_coupling_map.py
+++ packages/circuit/quri_parts/circuit/topology/cnot_coupling_map.py
@@ -67,8 +67,9 @@ def approx_cnot_reliable_subgraph(
 
 
 def graph_to_coupling_list(graph: nx.Graph) -> Sequence[tuple[int, ...]]:
-    """Convert networkx Graph (contains integer name nodes) to Sequence of integer tuples."""
-    return list(map(lambda qs: tuple(map(int, qs)), graph.edges)))
+    """Convert networkx Graph (contains integer name nodes) to Sequence of
+    integer tuples."""
+    return list(map(lambda qs: tuple(map(int, qs)), graph.edges))
 
 
 def _length_satisfactory_paths(
