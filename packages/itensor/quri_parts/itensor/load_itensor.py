73bde688d210e7fe54805446e1622726d7d56136
Toru Shibamiya <shibamiya@qunasys.com>
2023-10-13 17:08:21 +0900

    Add flag

--- packages/itensor/quri_parts/itensor/load_itensor.py
+++ packages/itensor/quri_parts/itensor/load_itensor.py
@@ -6,9 +6,14 @@ _ABS_DIR = os.path.dirname(os.path.abspath(_FILE))
 _LIBRARY_PATH = os.path.join(_ABS_DIR, "library.jl")
 _INCLUDE_STATEMENT = 'include("' + _LIBRARY_PATH + '")'
 
+_is_jl_library_included = False
+
 
 def ensure_itensor_loaded() -> None:
     """Ensure that the ITensor library is loaded into Juliacall."""
-    from juliacall import Main as jl
+    global _is_jl_library_included
 
-    jl.seval(_INCLUDE_STATEMENT)
+    if not _is_jl_library_included:
+        from juliacall import Main as jl
+        jl.seval(_INCLUDE_STATEMENT)
+        _is_jl_library_included = True
