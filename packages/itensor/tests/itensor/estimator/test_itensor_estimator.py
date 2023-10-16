93b931a43bd53f9296610001e254862b2f92a117
Toru Shibamiya <shibamiya@qunasys.com>
2023-10-16 09:40:45 +0900

    Fix tests

--- packages/itensor/tests/itensor/estimator/test_itensor_estimator.py
+++ packages/itensor/tests/itensor/estimator/test_itensor_estimator.py
@@ -253,6 +253,6 @@ class TestITensorParametricEstimator:
         )
         estimate_with_kwargs = estimator_with_kwargs(operator, state, params)
         assert estimate_with_kwargs.value == pytest.approx(
-            0.024748751563436706 + 0.02891269249339678j
+            0.02485533900849108+0.029037213118795355j
         )
         assert np.isnan(estimate_with_kwargs.error)
