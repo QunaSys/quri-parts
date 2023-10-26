2d11f508dea83f1ee2456cc8abc3217309c05fbe
Daniel <chung@qunasys.com>
2023-10-26 09:44:36 +0800

    modify tests

--- packages/core/tests/core/operator/test_sparse.py
+++ packages/core/tests/core/operator/test_sparse.py
@@ -21,12 +21,6 @@ from quri_parts.core.operator import (
     pauli_label,
     zero,
 )
-from quri_parts.core.operator.sparse import (
-    _convert_operator_to_sparse,
-    _convert_pauli_label_to_sparse,
-    _convert_pauli_map_to_other_format,
-    _pauli_map,
-)
 
 I = np.eye(2, dtype=np.complex128)  # noqa: E741
 X = np.array([[0, 1], [1, 0]], dtype=np.complex128)
@@ -34,101 +28,88 @@ Y = np.array([[0, -1j], [1j, 0]], dtype=np.complex128)
 Z = np.array([[1, 0], [0, -1]], dtype=np.complex128)
 
 
-def test_convert_pauli_map_to_other_format() -> None:
-    _convert_pauli_map_to_other_format("csc")
-    for p in _pauli_map.values():
-        assert isinstance(p, sparse.csc_matrix)
+class TestGetSparseMatrix:
+    def test_get_sparse_matrix_pauli_label(self) -> None:
+        test_pauli_label = pauli_label("X0 X1 Y3 Z4")
+        expected_matrix = reduce(np.kron, [Z, Y, I, X, X])
+        assert np.allclose(
+            get_sparse_matrix(test_pauli_label).toarray(), expected_matrix
+        )
+        assert isinstance(get_sparse_matrix(test_pauli_label), sparse.csc_matrix)
 
-    _convert_pauli_map_to_other_format("csr")
-    for p in _pauli_map.values():
-        assert isinstance(p, sparse.csr_matrix)
+    def test_get_sparse_matrix_sparse_matrix_type(self) -> None:
+        test_pauli_label = pauli_label("X0 X1 Y3 Z4")
+        assert isinstance(get_sparse_matrix(test_pauli_label), sparse.csc_matrix)
 
-    _convert_pauli_map_to_other_format("bsr")
-    for p in _pauli_map.values():
-        assert isinstance(p, sparse.bsr_matrix)
+        assert isinstance(
+            get_sparse_matrix(test_pauli_label, format="csr"), sparse.csr_matrix
+        )
 
-    _convert_pauli_map_to_other_format("dia")
-    for p in _pauli_map.values():
-        assert isinstance(p, sparse.dia_matrix)
+        assert isinstance(
+            get_sparse_matrix(test_pauli_label, format="bsr"), sparse.bsr_matrix
+        )
 
-    _convert_pauli_map_to_other_format("dok")
-    for p in _pauli_map.values():
-        assert isinstance(p, sparse.dok_matrix)
+        assert isinstance(
+            get_sparse_matrix(test_pauli_label, format="coo"), sparse.coo_matrix
+        )
 
-    _convert_pauli_map_to_other_format("lil")
-    for p in _pauli_map.values():
-        assert isinstance(p, sparse.lil_matrix)
+        assert isinstance(
+            get_sparse_matrix(test_pauli_label, format="dia"), sparse.dia_matrix
+        )
 
+        assert isinstance(
+            get_sparse_matrix(test_pauli_label, format="dok"), sparse.dok_matrix
+        )
 
-def test_convert_operator_to_sparse() -> None:
-    operator = Operator({pauli_label("X0 Y3"): -3, PAULI_IDENTITY: 8})
-    expected_matrix = -3 * reduce(np.kron, [Y, I, I, X]) + 8 * np.eye(16)
-    assert np.allclose(_convert_operator_to_sparse(operator).toarray(), expected_matrix)
+        assert isinstance(
+            get_sparse_matrix(test_pauli_label, format="lil"), sparse.lil_matrix
+        )
 
-    operator = zero()
-    assert np.allclose(
-        _convert_operator_to_sparse(operator, 3).toarray(), np.zeros((1, 1))
-    )
+    def test_get_sparse_matrix_nqubit_specified(self) -> None:
+        test_pauli_label = pauli_label("X0 X1 Y3 Z4")
+        expected_matrix = reduce(np.kron, [I, Z, Y, I, X, X])
+        assert np.allclose(
+            get_sparse_matrix(test_pauli_label, 6).toarray(), expected_matrix
+        )
 
-    operator = Operator({PAULI_IDENTITY: 8})
-    assert np.allclose(
-        _convert_operator_to_sparse(operator, 3).toarray(), 8 * np.eye(8)
-    )
+        with pytest.raises(
+            AssertionError,
+            match=(
+                "The specified number of qubits should not be less then the length"
+                " of the pauli operator."
+            ),
+        ):
+            get_sparse_matrix(test_pauli_label, 3)
 
-    with pytest.raises(ValueError):
-        _convert_operator_to_sparse(operator)
+    def test_get_sparse_matrix_for_operator(self) -> None:
+        operator = Operator({pauli_label("X0 Y3"): -3, PAULI_IDENTITY: 8})
+        expected_matrix = -3 * reduce(np.kron, [Y, I, I, X]) + 8 * np.eye(16)
+        assert np.allclose(get_sparse_matrix(operator).toarray(), expected_matrix)
 
+    def test_get_sparse_matrix_for_zero(self) -> None:
+        operator = zero()
+        converted = get_sparse_matrix(operator).toarray()
+        assert np.allclose(converted, np.zeros((1, 1)))
+        assert converted.shape == (1, 1)
 
-def test_convert_pauli_label_to_sparse() -> None:
-    test_pauli_label = pauli_label("X0 X1 Y3 Z4")
-    expected_matrix = reduce(np.kron, [Z, Y, I, X, X])
-    assert np.allclose(
-        _convert_pauli_label_to_sparse(test_pauli_label).toarray(), expected_matrix
-    )
+        operator = zero()
+        converted = get_sparse_matrix(operator, 3).toarray()
+        assert np.allclose(converted, np.zeros((8, 8)))
+        assert converted.shape == (8, 8)
 
-    with pytest.raises(
-        AssertionError,
-        match=(
-            "The specified number of qubits should not be less then the length"
-            " of the pauli operator."
-        ),
-    ):
-        _convert_pauli_label_to_sparse(test_pauli_label, 2).toarray()
+    def test_get_sparse_matrix_for_identity(self) -> None:
+        test_pauli_label = PAULI_IDENTITY
+        assert np.allclose(get_sparse_matrix(test_pauli_label, 3).toarray(), np.eye(8))
+        with pytest.raises(
+            AssertionError,
+            match=(
+                "n_qubits needs to be specified for PAULI_IDENTITY"
+                " to be converted to matrix."
+            ),
+        ):
+            get_sparse_matrix(test_pauli_label)
 
-    test_pauli_label = PAULI_IDENTITY
-    assert np.allclose(
-        _convert_pauli_label_to_sparse(test_pauli_label, 3).toarray(), np.eye(8)
-    )
-    with pytest.raises(
-        AssertionError,
-        match=(
-            "n_qubits needs to be specified for PAULI_IDENTITY"
-            " to be converted to matrix."
-        ),
-    ):
-        _convert_pauli_label_to_sparse(test_pauli_label)
-
-
-def test_get_sparse_matrix() -> None:
-    test_pauli_label = pauli_label("X0 X1 Y3 Z4")
-    expected_matrix = reduce(np.kron, [Z, Y, I, X, X])
-    assert np.allclose(get_sparse_matrix(test_pauli_label).toarray(), expected_matrix)
-    test_pauli_label = PAULI_IDENTITY
-    assert np.allclose(get_sparse_matrix(test_pauli_label, 3).toarray(), np.eye(8))
-
-    operator = Operator({pauli_label("X0 Y3"): -3, PAULI_IDENTITY: 8})
-    expected_matrix = -3 * reduce(np.kron, [Y, I, I, X]) + 8 * np.eye(16)
-    assert np.allclose(get_sparse_matrix(operator).toarray(), expected_matrix)
-
-    operator = zero()
-    converted = get_sparse_matrix(operator).toarray()
-    assert np.allclose(converted, np.zeros((1, 1)))
-    assert converted.shape == (1, 1)
-
-    operator = zero()
-    converted = get_sparse_matrix(operator, 3).toarray()
-    assert np.allclose(converted, np.zeros((8, 8)))
-    assert converted.shape == (8, 8)
-
-    operator = Operator({PAULI_IDENTITY: 8})
-    assert np.allclose(get_sparse_matrix(operator, 3).toarray(), 8 * np.eye(8))
+        operator = Operator({PAULI_IDENTITY: 8})
+        assert np.allclose(get_sparse_matrix(operator, 3).toarray(), 8 * np.eye(8))
+        with pytest.raises(ValueError):
+            get_sparse_matrix(operator)
