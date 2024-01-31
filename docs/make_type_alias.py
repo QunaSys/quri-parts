a5d89f3f6e11871b4614e62d7a01b96155ddc4b5
Daniel <chung@qunasys.com>
2024-01-31 10:05:40 +0800

    import line as first line of each files

--- docs/make_type_alias.py
+++ docs/make_type_alias.py
@@ -5,7 +5,7 @@ from typing import Callable
 from typing_extensions import TypeAlias
 
 DIR = r"../packages/"
-type_alias_recorder = {}
+TYPE_ALIAS_RECORDER = {}
 
 Action: TypeAlias = Callable[[str], None]
 
@@ -25,57 +25,28 @@ def iterate_line_to_find_type_alias(file_name: str) -> None:
         for line in f:
             if "TypeAlias" in line and "import" not in line:
                 alias = line.split(":")[0]
-                type_alias_recorder[alias] = alias
+                TYPE_ALIAS_RECORDER[alias] = alias
 
 
 def insert_future_annotation(file_name: str) -> None:
-    """insert "from __future__ import annotations" """
-    print(file_name)
+    """Insert "from __future__ import annotations"."""
     lines = []
-    import_text = "from __future__ import annotations"
-    license_text_last_line = "# limitations under the License."
+    import_text = "from __future__ import annotations\n"
 
     with open(file_name, "r") as f:
-        for l in f:
-            lines.append(l)
-    
-    if len(lines) == 0:
-        return
+        for line in f:
+            lines.append(line)
 
-    if import_text in [l.strip() for l in lines]:
-        return
+    lines.insert(0, import_text)
 
-    insert_line = 0
-    while insert_line < len(lines):
-        line_text = lines[insert_line]
-        if (
-            ("from" in line_text)
-            or ("import" in line_text)
-            or ("def" in line_text)
-            or ("class" in line_text)
-        ):
-            break
-        elif "=" in line_text:
-            while ("#" in lines[insert_line - 1]) and (
-                lines[insert_line - 1].strip() != license_text_last_line
-            ):
-                insert_line -= 1
-            break
-
-        insert_line += 1
-    else:
-        return
-
-    lines.insert(insert_line, import_text+"\n")
     new_line_text = "".join(lines)
-
     with open(file_name, "w") as f:
         f.write(new_line_text)
 
 
 if __name__ == "__main__":
-    iterate_files(DIR, iterate_line_to_find_type_alias)    
+    iterate_files(DIR, iterate_line_to_find_type_alias)
     with open("qp_type_aliases.json", "w") as f:
-        json.dump(type_alias_recorder, fp=f)
+        json.dump(TYPE_ALIAS_RECORDER, fp=f)
 
     iterate_files(DIR, insert_future_annotation)
