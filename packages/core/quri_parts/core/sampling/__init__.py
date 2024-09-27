38f396719cf5b55a64652870b22fcb5f84b4714d
Daniel <chung@qunasys.com>
2024-09-27 09:26:54 +0800

    parametric sampler and general sampler

--- packages/core/quri_parts/core/sampling/__init__.py
+++ packages/core/quri_parts/core/sampling/__init__.py
@@ -9,15 +9,19 @@
 # limitations under the License.
 
 from collections import Counter
+from dataclasses import dataclass, field
 from typing import (
+    Any,
     Callable,
     Collection,
+    Generic,
     Iterable,
     Mapping,
     NamedTuple,
     Sequence,
     TypeVar,
     Union,
+    cast,
 )
 
 import numpy as np
@@ -25,15 +29,32 @@ import numpy.typing as npt
 from typing_extensions import TypeAlias
 
 from quri_parts.backend import SamplingBackend
-from quri_parts.circuit import ImmutableQuantumCircuit
+from quri_parts.circuit import (
+    ImmutableQuantumCircuit,
+    UnboundParametricQuantumCircuitProtocol,
+)
 from quri_parts.core.operator import CommutablePauliSet, Operator
-from quri_parts.core.state import CircuitQuantumState, QuantumStateVector
+from quri_parts.core.state import (
+    CircuitQuantumState,
+    ParametricCircuitQuantumState,
+    ParametricQuantumStateVector,
+    QuantumStateVector,
+)
 
 #: A type variable represents *any* non-parametric quantum state classes.
 #: This is different from :class:`quri_parts.core.state.QuantumStateT`;
 #: ``QuantumStateT`` represents *either one of* the classes, while ``_StateT`` also
 #: covers *a union of* multiple state classes.
 _StateT = TypeVar("_StateT", bound=Union[CircuitQuantumState, QuantumStateVector])
+
+#: A type variable represents *any* parametric quantum state classes.
+#: This is different from :class:`quri_parts.core.state.ParametricQuantumStateT`;
+#: ``ParametricQuantumStateT`` represents *either one of* the classes, while ``_ParametricStateT``
+#: also covers *a union of* multiple state classes.
+_ParametricStateT = TypeVar(
+    "_ParametricStateT",
+    bound=Union[ParametricCircuitQuantumState, ParametricQuantumStateVector],
+)
 
 #: MeasurementCounts represents count statistics of repeated measurements of a quantum
 #: circuit. Keys are observed bit patterns encoded in integers and values are counts
@@ -51,6 +72,22 @@ ConcurrentSampler: TypeAlias = Callable[
     [Iterable[tuple[ImmutableQuantumCircuit, int]]], Iterable[MeasurementCounts]
 ]
 
+
+#: ParametricSampler represents a sampler that samples from a parametric circuit with
+#: a fixed set of circuit parameters.
+ParametricSampler: TypeAlias = Callable[
+    [UnboundParametricQuantumCircuitProtocol, int, Sequence[float]], MeasurementCounts
+]
+
+
+#: ConcurrentParametricSampler represents a sampler that samples from a parametric
+#: circuit with a sequence of circuit parameter list.
+ConcurrentParametricSampler: TypeAlias = Callable[
+    [Iterable[tuple[UnboundParametricQuantumCircuitProtocol, int, Sequence[float]]]],
+    Iterable[MeasurementCounts],
+]
+
+
 #: StateSampler representes a function that samples a specific (non-parametric) state by
 #: specified times and returns the count statistics. In the case of an ideal
 #: StateSampler, the return value corresponds to probabilities multiplied by shot count.
@@ -61,6 +98,132 @@ StateSampler: TypeAlias = Callable[[_StateT, int], MeasurementCounts]
 ConcurrentStateSampler: TypeAlias = Callable[
     [Iterable[tuple[_StateT, int]]], Iterable[MeasurementCounts]
 ]
+
+
+#: ParametricStateSampler represents a state sampler that samples from a
+#: parametric state with a fixed set of circuit parameters.
+ParametricStateSampler: TypeAlias = Callable[
+    [_ParametricStateT, int, Sequence[float]], MeasurementCounts
+]
+
+
+#: ConcurrentParametricStateSampler represents a state sampler that samples from a
+#: parametric state with a sequence of circuit parameter list.
+ConcurrentParametricStateSampler: TypeAlias = Callable[
+    [Iterable[tuple[_ParametricStateT, int, Sequence[float]]]],
+    Iterable[MeasurementCounts],
+]
+
+
+@dataclass
+class GeneralSampler(Generic[_StateT, _ParametricStateT]):
+    sampler: Sampler
+    state_sampler: StateSampler[_StateT]
+    parametric_sampler: ParametricSampler = field(init=False)
+    parametric_state_sampler: ParametricStateSampler = field(init=False)
+
+    def __post_init__(self) -> None:
+        self.parametric_sampler = create_parametric_sampler_from_sampler(self.sampler)
+        self.parametric_state_sampler = (
+            create_parametric_state_sampler_from_state_sampler(self.state_sampler)
+        )
+
+    def __call__(
+        self, *sampler_input: Any
+    ) -> Union[MeasurementCounts, Iterable[MeasurementCounts]]:
+        if isinstance(sampler_input[0], NonParametricQuantumCircuit):
+            assert isinstance(sampler_input[1], int), "Shot is not specified properly."
+            return self.sampler(*sampler_input)
+        if isinstance(sampler_input[0], UnboundParametricQuantumCircuitProtocol):
+            assert isinstance(sampler_input[1], int), "Shot is not specified properly."
+            assert isinstance(
+                sampler_input[2], Iterable
+            ), "Circuit parameter is not specified properly."
+            return self.parametric_sampler(*sampler_input)
+        if isinstance(sampler_input[0], (CircuitQuantumState, QuantumStateVector)):
+            assert isinstance(sampler_input[1], int), "Shot is not specified properly."
+            return self.state_sampler(*sampler_input)
+        if isinstance(
+            sampler_input[0],
+            (ParametricCircuitQuantumState, ParametricQuantumStateVector),
+        ):
+            assert isinstance(sampler_input[1], int), "Shot is not specified properly."
+            return self.parametric_state_sampler(*sampler_input)
+
+        if isinstance(sampler_input[0][0], Iterable):
+            return self(*sampler_input[0])
+
+        return [self(*comb) for comb in sampler_input]
+
+
+def create_parametric_sampler_from_sampler(sampler: Sampler) -> ParametricSampler:
+    """Create a :class:`ParametricSampler` from a :class:`Sampler`."""
+
+    def _parametric_sampler(
+        param_circuit: UnboundParametricQuantumCircuitProtocol,
+        measurement_cnt: int,
+        param: Sequence[float],
+    ) -> MeasurementCounts:
+        bound_circuit = param_circuit.bind_parameters(param)
+        return sampler(bound_circuit, measurement_cnt)
+
+    return _parametric_sampler
+
+
+def create_concurrent_parametric_sampler_from_concurrent_sampler(
+    concurrent_sampler: ConcurrentSampler,
+) -> ConcurrentParametricSampler:
+    """Create a :class:`ConcurrentParametricSampler` from a :class:`ConcurrentSampler`."""
+
+    def _concurrent_parametric_sampler(
+        circuit_shot_param_tuples: Iterable[
+            tuple[UnboundParametricQuantumCircuitProtocol, int, Sequence[float]]
+        ]
+    ) -> Iterable[MeasurementCounts]:
+        circuit_shot_tuples = [
+            (circuit.bind_parameters(param), shot)
+            for circuit, shot, param in circuit_shot_param_tuples
+        ]
+        return concurrent_sampler(circuit_shot_tuples)
+
+    return _concurrent_parametric_sampler
+
+
+def create_parametric_state_sampler_from_state_sampler(
+    state_sampler: StateSampler[_StateT],
+) -> ParametricStateSampler[_ParametricStateT]:
+    """Create a :class:`ParametricStateSampler` from a :class:`StateSampler`."""
+
+    def _parametric_state_sampler(
+        param_state: _ParametricStateT,
+        measurement_cnt: int,
+        param: Sequence[float],
+    ) -> MeasurementCounts:
+        bound_state = cast(_StateT, param_state.bind_parameters(param))
+        return state_sampler(bound_state, measurement_cnt)
+
+    return _parametric_state_sampler
+
+
+def create_concurrent_parametric_state_sampler_from_concurrent_state_sampler(
+    concurrent_state_sampler: ConcurrentStateSampler[_StateT],
+) -> ConcurrentParametricStateSampler[_ParametricStateT]:
+    """Create a :class:`ConcurrentParametricStateSampler` from a
+    :class:`ConcurrentStateSampler`.
+    """
+
+    def _concurrent_parametric_state_sampler(
+        state_shot_param_tuples: Iterable[
+            tuple[_ParametricStateT, int, Sequence[float]]
+        ]
+    ) -> Iterable[MeasurementCounts]:
+        state_shot_tuples = [
+            (cast(_StateT, state.bind_parameters(param)), shot)
+            for state, shot, param in state_shot_param_tuples
+        ]
+        return concurrent_state_sampler(state_shot_tuples)
+
+    return _concurrent_parametric_state_sampler
 
 
 def sample_from_state_vector(
--- packages/core/tests/core/sampling/test_create_sampler.py
+++ packages/core/tests/core/sampling/test_create_sampler.py
@@ -8,15 +8,294 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from collections.abc import Sequence
-from unittest import mock
+from collections.abc import Iterable, Sequence
+from typing import Union
+from unittest import TestCase, mock
 
-from quri_parts.circuit import QuantumCircuit
+import numpy as np
+
+from quri_parts.circuit import (
+    CONST,
+    LinearMappedUnboundParametricQuantumCircuit,
+    NonParametricQuantumCircuit,
+    QuantumCircuit,
+    UnboundParametricQuantumCircuit,
+)
 from quri_parts.core.sampling import (
+    GeneralSampler,
     MeasurementCounts,
+    create_concurrent_parametric_sampler_from_concurrent_sampler,
+    create_concurrent_parametric_state_sampler_from_concurrent_state_sampler,
     create_concurrent_sampler_from_sampling_backend,
+    create_parametric_sampler_from_sampler,
+    create_parametric_state_sampler_from_state_sampler,
     create_sampler_from_sampling_backend,
 )
+from quri_parts.core.state import CircuitQuantumState, QuantumStateVector, quantum_state
+
+
+def fake_sampler(circuit: NonParametricQuantumCircuit, shot: int) -> MeasurementCounts:
+    cnt = 0
+    for g in circuit.gates:
+        cnt += sum(g.params)
+    return {0: int(cnt) * shot}
+
+
+def fake_concurrent_sampler(
+    circuit_shot_pairs: Iterable[tuple[NonParametricQuantumCircuit, int]]
+) -> Iterable[MeasurementCounts]:
+    return [fake_sampler(c, s) for c, s in circuit_shot_pairs]
+
+
+def fake_state_sampler(
+    state: Union[CircuitQuantumState, QuantumStateVector], shot: int
+) -> MeasurementCounts:
+    cnt = 0
+    for g in state.circuit.gates:
+        cnt += sum(g.params)
+    if isinstance(state, QuantumStateVector):
+        cnt *= int(np.linalg.norm(state.vector, 1))
+    return {0: int(cnt) * shot}
+
+
+def fake_concurrent_state_sampler(
+    state_shot_pairs: Iterable[
+        tuple[Union[CircuitQuantumState, QuantumStateVector], int]
+    ]
+) -> Iterable[MeasurementCounts]:
+    return [fake_state_sampler(c, s) for c, s in state_shot_pairs]
+
+
+class TestParametricSampler(TestCase):
+    param_circuit_1: UnboundParametricQuantumCircuit
+    param_circuit_2: LinearMappedUnboundParametricQuantumCircuit
+
+    def setUp(self) -> None:
+        self.param_circuit_1 = UnboundParametricQuantumCircuit(2)
+        self.param_circuit_1.add_ParametricRX_gate(0)
+        self.param_circuit_1.add_ParametricRZ_gate(1)
+
+        self.param_circuit_2 = LinearMappedUnboundParametricQuantumCircuit(2)
+        a, b = self.param_circuit_2.add_parameters("a", "b")
+        self.param_circuit_2.add_ParametricRX_gate(0, {a: -1, b: -2, CONST: -3})
+        self.param_circuit_2.add_ParametricRZ_gate(1, {a: 4, b: 5, CONST: 6})
+
+        self.param_state_1 = quantum_state(2, circuit=self.param_circuit_1)
+        self.param_state_2 = quantum_state(
+            2, circuit=self.param_circuit_2, vector=np.ones(4) / 2
+        )
+
+    def test_create_parametric_sampler_from_sampler(self) -> None:
+        sampler = fake_sampler
+        param_sampler = create_parametric_sampler_from_sampler(sampler)
+
+        assert param_sampler(self.param_circuit_1, 1000, [1, 2]) == {0: 3000}
+        assert param_sampler(self.param_circuit_1, 2000, [3, 4]) == {0: 14000}
+
+        assert param_sampler(self.param_circuit_2, 1000, [1, 2]) == {
+            0: (-8 + 20) * 1000
+        }
+        assert param_sampler(self.param_circuit_2, 2000, [3, 4]) == {
+            0: (-14 + 38) * 2000
+        }
+
+    def test_create_concurrent_parametric_sampler_from_concurrent_sampler(self) -> None:
+        concurrent_sampler = fake_concurrent_sampler
+        concurrent_param_sampler = (
+            create_concurrent_parametric_sampler_from_concurrent_sampler(
+                concurrent_sampler
+            )
+        )
+
+        assert concurrent_param_sampler(
+            [
+                (self.param_circuit_1, 1000, [1, 2]),
+                (self.param_circuit_1, 2000, [3, 4]),
+                (self.param_circuit_2, 1000, [1, 2]),
+                (self.param_circuit_2, 2000, [3, 4]),
+            ],
+        ) == [{0: 3000}, {0: 14000}, {0: (-8 + 20) * 1000}, {0: (-14 + 38) * 2000}]
+
+    def test_create_parametric_state_sampler_from_state_sampler(self) -> None:
+        state_sampler = fake_state_sampler
+        parametric_state_sampler = create_parametric_state_sampler_from_state_sampler(
+            state_sampler
+        )
+        assert parametric_state_sampler(self.param_state_1, 1000, [1, 2]) == {0: 3000}
+        assert parametric_state_sampler(self.param_state_1, 2000, [3, 4]) == {0: 14000}
+        assert parametric_state_sampler(self.param_state_2, 1000, [1, 2]) == {
+            0: (-8 + 20) * 1000 * 2
+        }
+        assert parametric_state_sampler(self.param_state_2, 2000, [3, 4]) == {
+            0: (-14 + 38) * 2000 * 2
+        }
+
+    def test_create_concurrent_parametric_state_sampler_from_concurrent_state_sampler(
+        self,
+    ) -> None:
+        concurrent_state_sampler = fake_concurrent_state_sampler
+        concurrent_parametric_state_sampler = (
+            create_concurrent_parametric_state_sampler_from_concurrent_state_sampler(
+                concurrent_state_sampler
+            )
+        )
+        assert concurrent_parametric_state_sampler(
+            [
+                (self.param_state_1, 1000, [1, 2]),
+                (self.param_state_1, 2000, [3, 4]),
+                (self.param_state_2, 1000, [1, 2]),
+                (self.param_state_2, 2000, [3, 4]),
+            ]
+        ) == [
+            {0: 3000},
+            {0: 14000},
+            {0: (-8 + 20) * 1000 * 2},
+            {0: (-14 + 38) * 2000 * 2},
+        ]
+
+
+class TestGeneralSampler(TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.param_circuit_1 = UnboundParametricQuantumCircuit(2)
+        cls.param_circuit_1.add_ParametricRX_gate(0)
+        cls.param_circuit_1.add_ParametricRZ_gate(1)
+
+        cls.param_circuit_2 = LinearMappedUnboundParametricQuantumCircuit(2)
+        a, b = cls.param_circuit_2.add_parameters("a", "b")
+        cls.param_circuit_2.add_ParametricRX_gate(0, {a: -1, b: -2, CONST: -3})
+        cls.param_circuit_2.add_ParametricRZ_gate(1, {a: 4, b: 5, CONST: 6})
+
+        cls.param_state_1 = quantum_state(2, circuit=cls.param_circuit_1)
+        cls.param_state_2 = quantum_state(
+            2, circuit=cls.param_circuit_2, vector=np.ones(4) / 2
+        )
+
+        cls.general_sampler = GeneralSampler(
+            sampler=fake_sampler, state_sampler=fake_state_sampler
+        )
+
+    def test_sampler_input(self) -> None:
+        circuit = self.param_circuit_1.bind_parameters([1, 2])
+        assert self.general_sampler(circuit, 1000) == {0: 3000}
+        circuit = self.param_circuit_1.bind_parameters([3, 4])
+        assert self.general_sampler(circuit, 2000) == {0: 14000}
+        circuit = self.param_circuit_2.bind_parameters([1, 2])
+        assert self.general_sampler(circuit, 1000) == {0: (-8 + 20) * 1000}
+        circuit = self.param_circuit_2.bind_parameters([3, 4])
+        assert self.general_sampler(circuit, 2000) == {0: (-14 + 38) * 2000}
+
+    def test_concurrent_sampler_input(self) -> None:
+        circuit_1 = self.param_circuit_1.bind_parameters([1, 2])
+        circuit_2 = self.param_circuit_1.bind_parameters([3, 4])
+        assert self.general_sampler((circuit_1, 1000), (circuit_2, 2000)) == [
+            {0: 3000},
+            {0: 14000},
+        ]
+
+        assert self.general_sampler([(circuit_1, 1000), (circuit_2, 2000)]) == [
+            {0: 3000},
+            {0: 14000},
+        ]
+
+    def test_param_sampler_input(self) -> None:
+        assert self.general_sampler(self.param_circuit_1, 1000, [1, 2]) == {0: 3000}
+        assert self.general_sampler(self.param_circuit_1, 2000, [3, 4]) == {0: 14000}
+        assert self.general_sampler(self.param_circuit_2, 1000, [1, 2]) == {
+            0: (-8 + 20) * 1000
+        }
+        assert self.general_sampler(self.param_circuit_2, 2000, [3, 4]) == {
+            0: (-14 + 38) * 2000
+        }
+
+    def test_concurrent_param_sampler_input(self) -> None:
+        assert self.general_sampler(
+            (self.param_circuit_1, 1000, [1, 2]),
+            (self.param_circuit_1, 2000, [3, 4]),
+            (self.param_circuit_2, 1000, [1, 2]),
+            (self.param_circuit_2, 2000, [3, 4]),
+        ) == [{0: 3000}, {0: 14000}, {0: (-8 + 20) * 1000}, {0: (-14 + 38) * 2000}]
+
+        assert self.general_sampler(
+            [
+                (self.param_circuit_1, 1000, [1, 2]),
+                (self.param_circuit_1, 2000, [3, 4]),
+                (self.param_circuit_2, 1000, [1, 2]),
+                (self.param_circuit_2, 2000, [3, 4]),
+            ]
+        ) == [{0: 3000}, {0: 14000}, {0: (-8 + 20) * 1000}, {0: (-14 + 38) * 2000}]
+
+    def test_state_sampler_input(self) -> None:
+        state = self.param_state_1.bind_parameters([1, 2])
+        assert self.general_sampler(state, 1000) == {0: 3000}
+        state = self.param_state_1.bind_parameters([3, 4])
+        assert self.general_sampler(state, 2000) == {0: 14000}
+        state = self.param_state_2.bind_parameters([1, 2])
+        assert self.general_sampler(state, 1000) == {0: (-8 + 20) * 1000 * 2}
+        state = self.param_state_2.bind_parameters([3, 4])
+        assert self.general_sampler(state, 2000) == {0: (-14 + 38) * 2000 * 2}
+
+    def test_concurrent_state_sampler_input(self) -> None:
+        state_1 = self.param_state_1.bind_parameters([1, 2])
+        state_2 = self.param_state_1.bind_parameters([3, 4])
+        assert self.general_sampler((state_1, 1000), (state_2, 2000)) == [
+            {0: 3000},
+            {0: 14000},
+        ]
+        assert self.general_sampler([(state_1, 1000), (state_2, 2000)]) == [
+            {0: 3000},
+            {0: 14000},
+        ]
+
+    def test_param_state_sampler_input(self) -> None:
+        assert self.general_sampler(self.param_state_1, 1000, [1, 2]) == {0: 3000}
+        assert self.general_sampler(self.param_state_1, 2000, [3, 4]) == {0: 14000}
+        assert self.general_sampler(self.param_state_2, 1000, [1, 2]) == {
+            0: (-8 + 20) * 1000 * 2
+        }
+        assert self.general_sampler(self.param_state_2, 2000, [3, 4]) == {
+            0: (-14 + 38) * 2000 * 2
+        }
+
+    def test_concurrent_param_state_sampler_input(self) -> None:
+        assert self.general_sampler(
+            (self.param_state_1, 1000, [1, 2]),
+            (self.param_state_1, 2000, [3, 4]),
+            (self.param_state_2, 1000, [1, 2]),
+            (self.param_state_2, 2000, [3, 4]),
+        ) == [
+            {0: 3000},
+            {0: 14000},
+            {0: (-8 + 20) * 1000 * 2},
+            {0: (-14 + 38) * 2000 * 2},
+        ]
+
+        assert self.general_sampler(
+            [
+                (self.param_state_1, 1000, [1, 2]),
+                (self.param_state_1, 2000, [3, 4]),
+                (self.param_state_2, 1000, [1, 2]),
+                (self.param_state_2, 2000, [3, 4]),
+            ]
+        ) == [
+            {0: 3000},
+            {0: 14000},
+            {0: (-8 + 20) * 1000 * 2},
+            {0: (-14 + 38) * 2000 * 2},
+        ]
+
+    def test_mixed_concurrent_input(self) -> None:
+        assert self.general_sampler(
+            (self.param_circuit_1.bind_parameters([1, 2]), 1000),
+            (self.param_circuit_1, 2000, [3, 4]),
+            (self.param_state_2.bind_parameters([1, 2]), 1000),
+            (self.param_state_2, 2000, [3, 4]),
+        ) == [
+            {0: 3000},
+            {0: 14000},
+            {0: (-8 + 20) * 1000 * 2},
+            {0: (-14 + 38) * 2000 * 2},
+        ]
 
 
 def create_mock_backend(counts: Sequence[MeasurementCounts]) -> mock.Mock:
