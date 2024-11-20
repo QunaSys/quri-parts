34e84d71d019bfe07a369c180f35b5bac17678e1
Daito Sato <daito.sato.phys@gmail.com>
2024-11-20 10:52:06 +0900

    Update the file

--- packages/algo/quri_parts/algo/ansatz/Z2_Symmetry_Preserving.py
+++ packages/algo/quri_parts/algo/ansatz/Z2_Symmetry_Preserving.py
@@ -10,7 +10,7 @@ from quri_parts.circuit import (
     QuantumGate,
 )
 
-from .two_local import EntLayerMakerArg, RotLayerMakerArg, TwoLocal
+from .two_local import EntLayerMakerArg, TwoLocal
 
 
 def Rxx_gate(target_indices: Sequence[int], angle: float) -> QuantumGate:
