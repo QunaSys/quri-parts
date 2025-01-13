91247fd66bf6702356a9837a392c9c19c259d193
Daniel <chung@qunasys.com>
2025-01-13 17:15:26 +0800

    fixed time step

--- quri_algo/circuit/time_evolution/trotter_time_evo.py
+++ quri_algo/circuit/time_evolution/trotter_time_evo.py
@@ -11,13 +11,21 @@
 from functools import lru_cache
 from typing import Union
 
+import numpy as np
 from quri_parts.circuit import (
     ImmutableLinearMappedUnboundParametricQuantumCircuit,
     LinearMappedUnboundParametricQuantumCircuit,
     NonParametricQuantumCircuit,
+    QuantumCircuit,
+    inverse_circuit,
 )
 from quri_parts.circuit.transpile import CircuitTranspiler
-from quri_parts.core.operator import PAULI_IDENTITY, Operator, pauli_label
+from quri_parts.core.operator import (
+    PAULI_IDENTITY,
+    Operator,
+    pauli_label,
+    trotter_suzuki_decomposition,
+)
 
 from quri_algo.circuit.time_evolution.interface import TimeEvolutionCircuitFactory
 from quri_algo.circuit.utils.transpile import apply_transpiler
@@ -47,17 +55,32 @@ def get_trotter_time_evolution_operator(
     hamiltonian: Operator,
     n_state_qubits: int,
     n_trotter: int = 1,
+    trotter_order: int = 1,
 ) -> ImmutableLinearMappedUnboundParametricQuantumCircuit:
     circuit = LinearMappedUnboundParametricQuantumCircuit(n_state_qubits)
     t = circuit.add_parameter("t")
+
+    if trotter_order == 1:
+        for _ in range(n_trotter):
+            for op, coef in hamiltonian.items():
+                if op == PAULI_IDENTITY:
+                    continue
+                circuit.add_ParametricPauliRotation_gate(
+                    *op.index_and_pauli_id_list, {t: 2 * coef.real / n_trotter}
+                )
+        return circuit.freeze()
+
+    assert trotter_order % 2 == 0, "Trotter order must be 1 or an even number."
+    decomposition = trotter_suzuki_decomposition(hamiltonian, 1.0, trotter_order // 2)
+
     for _ in range(n_trotter):
-        for op, coef in hamiltonian.items():
-            if op == PAULI_IDENTITY:
+        for term in decomposition:
+            if term.pauli == PAULI_IDENTITY:
                 continue
             circuit.add_ParametricPauliRotation_gate(
-                *op.index_and_pauli_id_list, {t: 2 * coef.real / n_trotter}
+                *term.pauli.index_and_pauli_id_list,
+                {t: 2 * term.coefficient.real / n_trotter},
             )
-
     return circuit.freeze()
 
 
@@ -68,18 +91,21 @@ class TrotterTimeEvolutionCircuitFactory(
         self,
         encoded_problem: QubitHamiltonianInput,
         n_trotter: int,
+        trotter_order: int = 1,
         *,
         transpiler: CircuitTranspiler | None = None,
     ):
         self.n_trotter = n_trotter
         self.encoded_problem = encoded_problem
         self.qubit_count = self.encoded_problem.n_state_qubit
+        self.trotter_order = trotter_order
         self.transpiler = transpiler
         assert isinstance(self.encoded_problem, QubitHamiltonianInput)
         self._param_evo_circuit = get_trotter_time_evolution_operator(
             self.encoded_problem.qubit_hamiltonian,
             self.encoded_problem.n_state_qubit,
             self.n_trotter,
+            self.trotter_order,
         )
 
     @apply_transpiler  # type: ignore
@@ -126,6 +152,7 @@ class TrotterControlledTimeEvolutionCircuitFactory(
         self,
         encoded_problem: QubitHamiltonianInput,
         n_trotter: int,
+        trotter_order: int = 1,
         *,
         transpiler: CircuitTranspiler | None = None,
     ):
@@ -180,3 +207,47 @@ class TrotterPartialTimeEvolutionCircuitFactory(PartialTimeEvolutionCircuitFacto
         return (
             self.n_trotter
         )  # The arguments to a cached function must all be hashable, including `self`
+
+
+def get_evolution_trotter_step(
+    time_step: float, evolution_time: float
+) -> tuple[int, int]:
+    assert time_step > 0, "time step must be greater than 0."
+    step = np.abs(evolution_time) / time_step
+    if not np.isclose(np.round(step, 12), int(step)):
+        raise ValueError(
+            f"Evolution time {evolution_time} is not an integer "
+            f"muliple of time step {time_step}."
+        )
+    return int(np.round(step, 12)), int(np.sign(evolution_time))
+
+
+class FixedIntervalTrotterTimeEvolution(
+    TimeEvolutionCircuitFactory[QubitHamiltonianInput]
+):
+    def __init__(
+        self,
+        encoded_problem: QubitHamiltonianInput,
+        time_step: float,
+        trotter_order: int = 1,
+        *,
+        transpiler: CircuitTranspiler | None = None,
+    ):
+        self.encoded_problem = encoded_problem
+        self.qubit_count = encoded_problem.n_state_qubit
+        self.time_step = time_step
+        self.transpiler = transpiler
+
+        self._single_time_step_circuit = get_trotter_time_evolution_operator(
+            encoded_problem.qubit_hamiltonian,
+            encoded_problem.n_state_qubit,
+            trotter_order=trotter_order,
+        ).bind_parameters([time_step])
+
+    @apply_transpiler  # type: ignore
+    def __call__(self, evolution_time: float) -> NonParametricQuantumCircuit:
+        n_repition, sgn = get_evolution_trotter_step(self.time_step, evolution_time)
+        circuit = QuantumCircuit(self.qubit_count)
+        for _ in range(n_repition):
+            circuit.extend(self._single_time_step_circuit)
+        return circuit if sgn > 0 else inverse_circuit(circuit)
--- tests/circuit/time_evolution/test_trotter_circuit.py
+++ tests/circuit/time_evolution/test_trotter_circuit.py
@@ -8,14 +8,49 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import pytest
 from quri_parts.circuit import QuantumCircuit
-from quri_parts.core.operator import PAULI_IDENTITY, Operator, pauli_label
+from quri_parts.core.operator import PAULI_IDENTITY, Operator, PauliLabel, pauli_label
 
 from quri_algo.circuit.time_evolution.trotter_time_evo import (
+    FixedIntervalTrotterTimeEvolution,
     TrotterControlledTimeEvolutionCircuitFactory,
     TrotterTimeEvolutionCircuitFactory,
+    get_trotter_time_evolution_operator,
 )
 from quri_algo.problem import QubitHamiltonianInput
+
+
+def test_get_trotter_time_evolution_operator() -> None:
+    operator = Operator(
+        {pauli_label("X0 X1"): 2, pauli_label("Y0 Y1"): 3, PAULI_IDENTITY: 2}
+    )
+
+    # Trotter order 1
+    trotter_unbound_circuit = get_trotter_time_evolution_operator(
+        operator, n_state_qubits=2, n_trotter=1
+    )
+    trotter_circuit = trotter_unbound_circuit.bind_parameters([2.0])
+    reconstructed_op = Operator({})
+    for g in trotter_circuit.gates:
+        reconstructed_op.add_term(
+            PauliLabel.from_index_and_pauli_list(g.target_indices, g.pauli_ids),
+            0.5 * g.params[0],
+        )
+    assert reconstructed_op == 2.0 * (operator - Operator({PAULI_IDENTITY: 2}))
+
+    # Trotter order 2
+    trotter_unbound_circuit = get_trotter_time_evolution_operator(
+        operator, n_state_qubits=2, n_trotter=1, trotter_order=2
+    )
+    trotter_circuit = trotter_unbound_circuit.bind_parameters([2.0])
+    reconstructed_op = Operator({})
+    for g in trotter_circuit.gates:
+        reconstructed_op.add_term(
+            PauliLabel.from_index_and_pauli_list(g.target_indices, g.pauli_ids),
+            0.5 * g.params[0],
+        )
+    assert reconstructed_op == 2 * (operator - Operator({PAULI_IDENTITY: 2}))
 
 
 def test_trotter_factory() -> None:
@@ -37,6 +72,22 @@ def test_trotter_factory() -> None:
     for _ in range(n_trotter):
         expected_circuit.add_PauliRotation_gate([0, 1], [1, 1], 4.0 / n_trotter)
         expected_circuit.add_PauliRotation_gate([0, 1], [2, 2], 4.0 / n_trotter)
+    assert circuit == expected_circuit
+
+    trotter_order = 2
+    n_trotter = 3
+    circuit_factory = TrotterTimeEvolutionCircuitFactory(
+        problem, n_trotter, trotter_order
+    )
+    circuit = circuit_factory(1.0)
+
+    expected_circuit = QuantumCircuit(2)
+    for _ in range(n_trotter):
+        expected_circuit.add_PauliRotation_gate([0, 1], [1, 1], 2.0 / n_trotter)
+        expected_circuit.add_PauliRotation_gate([0, 1], [2, 2], 2.0 / n_trotter)
+        expected_circuit.add_PauliRotation_gate([0, 1], [2, 2], 2.0 / n_trotter)
+        expected_circuit.add_PauliRotation_gate([0, 1], [1, 1], 2.0 / n_trotter)
+
     assert circuit == expected_circuit
 
 
@@ -66,3 +117,35 @@ def test_controlled_trotter_factory() -> None:
         expected_circuit.add_PauliRotation_gate([0, 1, 2], [3, 2, 2], -2.0 / n_trotter)
         expected_circuit.add_RZ_gate(0, -2.0 / n_trotter)
     assert circuit == expected_circuit
+
+
+def test_fixed_interval_trotter() -> None:
+    operator = Operator(
+        {pauli_label("X0 X1"): 2, pauli_label("Y0 Y1"): 2, PAULI_IDENTITY: 2}
+    )
+    problem = QubitHamiltonianInput(2, operator)
+    time_step = 0.1
+    circuit_factory = FixedIntervalTrotterTimeEvolution(
+        problem, time_step, trotter_order=2
+    )
+    evolution_time = 0.5
+    circuit = circuit_factory(evolution_time)
+
+    single_step_circuit = QuantumCircuit(2)
+    single_step_circuit.add_PauliRotation_gate([0, 1], [1, 1], 2 * time_step)
+    single_step_circuit.add_PauliRotation_gate([0, 1], [2, 2], 2 * time_step)
+    single_step_circuit.add_PauliRotation_gate([0, 1], [2, 2], 2 * time_step)
+    single_step_circuit.add_PauliRotation_gate([0, 1], [1, 1], 2 * time_step)
+
+    expected = QuantumCircuit(2)
+    for _ in range(int(evolution_time / time_step)):
+        expected.extend(single_step_circuit)
+
+    assert expected == circuit
+
+    evolution_time = 0.43
+    with pytest.raises(
+        ValueError,
+        match=f"Evolution time {evolution_time} is not an integer muliple of time step {time_step}."
+    ):
+        circuit_factory(0.43)
